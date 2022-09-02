12a55963c74f11f6cb3d57cbd8f64d6398ee3e09
Kris Dyson <kristan@hotmail.co.uk>
2022-09-02 17:32:10 +0100

    Nav bar links

---
+++ src/UKMCAB.Web.UI/Ext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace UKMCAB.Web.UI;
+
+public static class Ext
+{
+    public static IHtmlContent Conditional(this IHtmlHelper htmlHelper, bool condition, string text) => condition ? htmlHelper.Raw(text) : HtmlString.Empty;
+
+    public static IHtmlContent Conditional(this IHtmlHelper htmlHelper, bool condition, HtmlString html) => condition ? html : HtmlString.Empty;
+
+    public static IHtmlContent Conditional<T>(this IHtmlHelper<T> htmlHelper, bool condition, string text) => condition ? htmlHelper.Raw(text) : HtmlString.Empty;
+
+    public static IHtmlContent Conditional<T>(this IHtmlHelper<T> htmlHelper, bool condition, HtmlString html) => condition ? html : HtmlString.Empty;
+}
