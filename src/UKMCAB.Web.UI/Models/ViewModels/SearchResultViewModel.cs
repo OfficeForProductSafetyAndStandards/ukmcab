db87041ff4dd8375af3e5fc542558ff0f06e0f9c
Andy Rose <andy@cyberian.co.uk>
2022-09-29 11:34:46 +0100

    Initial draft of search results changes

--- src/UKMCAB.Web.UI/Models/ViewModels/SearchResultViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/SearchResultViewModel.cs
@@ -5,6 +5,9 @@ public class SearchResultViewModel
     public string id { get; set; }
     public string Name { get; set; }
     public string Address { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public string Website { get; set; }
     public string Blurb { get; set; }
     public string BodyType { get; set; }
     public string RegisteredOfficeLocation { get; set; }
