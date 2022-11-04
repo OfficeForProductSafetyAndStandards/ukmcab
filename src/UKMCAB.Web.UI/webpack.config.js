efb7c188e1752d504885ccc0309f703e1222babd
Andy Rose <andy@cyberian.co.uk>
2022-11-04 11:06:28 +0000

    webpack update

--- src/UKMCAB.Web.UI/webpack.config.js
+++ src/UKMCAB.Web.UI/webpack.config.js
@@ -2,61 +2,50 @@ const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-	devtool: "source-map",
-	mode: "development",
-	entry: {
+    devtool: "source-map",
+    mode: "development",
+    entry: {
         main: "./assets/js/main.js"
     },
-	output: {
-		filename: "[name].js",
-		path: path.resolve(__dirname, "wwwroot/assets/js"),
-	},
-	module: {
-		rules: [
+    output: {
+        filename: "[name].js",
+        path: path.resolve(__dirname, "wwwroot/assets/js"),
+    },
+    module: {
+        rules: [
             {
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: ["@babel/preset-env"],
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"],
                     },
                 },
-			},
+            },
             {
-				test: /\.(sa|sc|c)ss$/,
-				exclude: /node_modules/,
-				use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
-			},
-			{
+                test: /\.(sa|sc|c)ss$/,
+                exclude: /node_modules/,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+            },
+            {
                 test: /\.(jpg|png|gif)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../img/',
-						publicPath: '../img/'
-                    }
-                }]
-			},
-			{
+                type: 'asset/resource',
+                dependency: { not: ['url'] },
+            },
+            {
                 test: /\.(woff2?|ttf|otf|eot|svg)$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: '../fonts/'
-                    }
-                }]
+                type: 'asset/resource',
+                dependency: { not: ['url'] },
             }
         ],
-	},
-	plugins: [
-		    new MiniCssExtractPlugin({
-				filename: "../css/site.css",
-            })
-	],
-	stats: {
-		errorDetails: true,
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "../css/site.css",
+        })
+    ],
+    stats: {
+        errorDetails: true,
     }
 };
