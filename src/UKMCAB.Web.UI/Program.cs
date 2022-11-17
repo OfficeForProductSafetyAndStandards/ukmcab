770ee75db84eb800f752d0840cb918535887dfe5
Andy Rose <andy@cyberian.co.uk>
2022-11-17 14:18:15 +0000

    Adding missing code line deleted in last merge

--- src/UKMCAB.Web.UI/Program.cs
+++ src/UKMCAB.Web.UI/Program.cs
@@ -34,8 +34,8 @@ if (!string.IsNullOrWhiteSpace(cosmosConnectionString))
     builder.Services.AddSingleton<ICosmosDbService>(new CosmosDbService(new CosmosClient(cosmosConnectionString), database, container));
 }
 
-
-
+builder.Services.Configure<IdentityStoresOptions>(options =>
+    options.UseAzureCosmosDB(cosmosConnectionString, databaseId: "UKMCABIdentity"));
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
     {
