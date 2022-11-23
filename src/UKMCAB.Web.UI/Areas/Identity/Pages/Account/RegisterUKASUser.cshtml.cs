9b77571cba6737004abad39c25b3fb339330bdb5
Andy Rose <andy@cyberian.co.uk>
2022-11-23 17:31:33 +0000

    New ukas registration page and confirmation handling

--- src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/RegisterUKASUser.cshtml.cs
+++ src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/RegisterUKASUser.cshtml.cs
@@ -1,18 +1,15 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-#nullable disable
-
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.WebUtilities;
 using System.ComponentModel.DataAnnotations;
 using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.WebUtilities;
 using UKMCAB.Identity.Stores.CosmosDB;
 
 namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
 {
-    public class RegisterOGDUserModel : PageModel, ILayoutModel
+    public class RegisterUKASUserModel : PageModel, ILayoutModel
     {
         private readonly SignInManager<UKMCABUser> _signInManager;
         private readonly UserManager<UKMCABUser> _userManager;
@@ -20,8 +17,9 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
         private readonly IUserEmailStore<UKMCABUser> _emailStore;
         private readonly ILogger<RegisterOGDUserModel> _logger;
         private readonly IEmailSender _emailSender;
+        public string? Title => "Register UKAS user";
 
-        public RegisterOGDUserModel(
+        public RegisterUKASUserModel(
             UserManager<UKMCABUser> userManager,
             IUserStore<UKMCABUser> userStore,
             SignInManager<UKMCABUser> signInManager,
@@ -36,22 +34,9 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             _emailSender = emailSender;
         }
 
-        public string? Title => "Register OGD user";
-
-        public List<string> LegislativeAreaList { get; set; }
-
-
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         [BindProperty]
         public InputModel Input { get; set; }
 
-        /// <summary>
-        ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
-        ///     directly from your code. This API may change or be removed in future releases.
-        /// </summary>
         public class InputModel
         {
             /// <summary>
@@ -60,7 +45,7 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             /// </summary>
             [Required]
             [EmailAddress]
-            [RegularExpression("^.+@.+(?:\\.gov\\.uk)$", ErrorMessage = "Only GOV UK email addresses can register for and OGD user account")]
+            [RegularExpression("^.+(?:\\@ukas\\.com)$", ErrorMessage = "Only ukas.com email addresses can register for and UKAS user account")]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
@@ -82,40 +67,27 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
-
-            [Required(ErrorMessage = "Please select at least one legislative area")]
-            [Display(Name = "Legislative areas")]
-            public List<string> LegislativeAreas { get; set; }
-
-            [Required(ErrorMessage = "Please provide a reason for the request")]
-            [Display(Name = "Reason for request")]
-            public string RequestReason { get; set; }
-
         }
 
 
         public async Task OnGetAsync()
         {
-            LegislativeAreaList = Constants.LegislativeAreas;
         }
 
         public async Task<IActionResult> OnPostAsync()
         {
-            var returnUrl = Url.Content("~/");
-            LegislativeAreaList = Constants.LegislativeAreas;
+             var returnUrl = Url.Content("~/");
             if (ModelState.IsValid)
             {
                 var user = CreateUser();
 
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
-                user.Regulations = Input.LegislativeAreas;
-                user.RequestReason = Input.RequestReason;
                 var result = await _userManager.CreateAsync(user, Input.Password);
-                var roleResult = await _userManager.AddToRoleAsync(user, Constants.Roles.OGDUser);
+                var roleResult = await _userManager.AddToRoleAsync(user, Constants.Roles.UKASUser);
                 if (result.Succeeded && roleResult.Succeeded)
                 {
-                    
+
                     _logger.LogInformation("User created a new account with password.");
 
                     var userId = await _userManager.GetUserIdAsync(user);
@@ -153,7 +125,6 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             // If we got this far, something failed, redisplay form
             return Page();
         }
-
         private UKMCABUser CreateUser()
         {
             try
@@ -163,8 +134,8 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             catch
             {
                 throw new InvalidOperationException($"Can't create an instance of '{nameof(UKMCABUser)}'. " +
-                    $"Ensure that '{nameof(UKMCABUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
-                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
+                                                    $"Ensure that '{nameof(UKMCABUser)}' is not an abstract class and has a parameterless constructor, or alternatively " +
+                                                    $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
         }
 
@@ -176,5 +147,6 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             }
             return (IUserEmailStore<UKMCABUser>)_userStore;
         }
+
     }
 }
