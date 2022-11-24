18ff61bcd553f781c79bb2cc6049bd798474339e
Andy Rose <andy@cyberian.co.uk>
2022-11-24 18:10:44 +0000

    Amendments to forgotten password form

--- src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
+++ src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/ForgotPassword.cshtml.cs
@@ -2,22 +2,17 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
-using System;
-using System.ComponentModel.DataAnnotations;
-using System.Text;
-using System.Text.Encodings.Web;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
+using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
 using UKMCAB.Identity.Stores.CosmosDB;
 
 namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
 {
-    public class ForgotPasswordModel : PageModel
+    public class ForgotPasswordModel : PageModel, ILayoutModel
     {
         private readonly UserManager<UKMCABUser> _userManager;
         private readonly IEmailSender _emailSender;
@@ -27,6 +22,8 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             _userManager = userManager;
             _emailSender = emailSender;
         }
+
+        public string? Title => "Forgotten password";
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -81,5 +78,6 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
 
             return Page();
         }
+
     }
 }
