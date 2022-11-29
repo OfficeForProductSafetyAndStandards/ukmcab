bcf7507db6a9cf0bf7b1f28c393dc6a05ae8ab06
Andy Rose <andy@cyberian.co.uk>
2022-11-29 17:33:18 +0000

    adding guard clause to redirect auth users to home page from login

--- src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ src/UKMCAB.Web.UI/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -79,8 +79,13 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             public bool RememberMe { get; set; }
         }
 
-        public async Task OnGetAsync(string returnUrl = null)
+        public async Task<IActionResult> OnGetAsync(string returnUrl = null)
         {
+            if (_signInManager.IsSignedIn(User))
+            {
+                return LocalRedirect("/");
+            }
+
             if (!string.IsNullOrEmpty(ErrorMessage))
             {
                 ModelState.AddModelError(string.Empty, ErrorMessage);
@@ -94,6 +99,8 @@ namespace UKMCAB.Web.UI.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
 
             ReturnUrl = returnUrl;
+
+            return Page();
         }
 
         public async Task<IActionResult> OnPostAsync(string returnUrl = null)
