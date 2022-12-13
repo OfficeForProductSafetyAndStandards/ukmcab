647c32aecea0c533a3eb5c3ea697e9c362ab6ceb
Andy Rose <andy@cyberian.co.uk>
2022-12-13 10:37:29 +0000

    More registration tests

--- src/UKMCAB.Test/Controllers/Accounts/RegisterControllerTests.cs
+++ src/UKMCAB.Test/Controllers/Accounts/RegisterControllerTests.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Microsoft.AspNetCore.DataProtection;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
 using Notify.Interfaces;
+using UKMCAB.Core.Models.Account;
 using UKMCAB.Core.Services.Account;
 using UKMCAB.Identity.Stores.CosmosDB;
 using UKMCAB.Web.UI;
@@ -24,7 +20,7 @@ namespace UKMCAB.Test.Controllers.Accounts
         private Mock<UserManager<UKMCABUser>> mockUserManager = new();
         private Mock<IPasswordValidator<UKMCABUser>> mockPasswordValidator;
         private Mock<IOptions<TemplateOptions>> mockTemplateOptions = new();
-        private new Mock<ITimeLimitedDataProtector> _dataProtector = new();
+        private Mock<IRegisterService> _mockRegisterService = new();
 
         [SetUp]
         public void Setup()
@@ -39,8 +35,7 @@ namespace UKMCAB.Test.Controllers.Accounts
             mockTemplateOptions.Setup(to => to.Value).Returns(new TemplateOptions { Register = "register", RegisterConfirmation = "register-confirmation"});
             mockAsyncNotificationClient = new();
 
-            var registerService = new RegisterService(_dataProtector.Object);
-            _sut = new RegisterController(registerService, mockAsyncNotificationClient.Object,
+            _sut = new RegisterController(_mockRegisterService.Object, mockAsyncNotificationClient.Object,
                 mockTemplateOptions.Object, mockUserManager.Object);
         }
 
@@ -104,6 +99,8 @@ namespace UKMCAB.Test.Controllers.Accounts
             mockPasswordValidator
                 .Setup(p => p.ValidateAsync(It.IsAny<UserManager<UKMCABUser>>(), It.IsAny<UKMCABUser>(),
                     It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+            _mockRegisterService.Setup(rs => rs.EncodeRegistrationDetails(It.IsAny<RegistrationDTO>()))
+                .Returns("encoded-string");
 
             TestHelper.SetupUrlActionReturn(_sut);
 
@@ -117,5 +114,88 @@ namespace UKMCAB.Test.Controllers.Accounts
             Assert.IsTrue(result.ActionName == nameof(RegisterController.RegisterConfirmation));
         }
 
+        [Test]
+        public async Task ConfirmRegistrationWithExistingEmailResultsInErrorMessage()
+        {
+            _mockRegisterService.Setup(rs => rs.DecodeRegistrationDetails("test-payload"))
+                .Returns(new RegistrationDTO { Email = "test@email.com" });
+            mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync(new UKMCABUser());
+
+            var result = await _sut.ConfirmEmail("test-payload") as ViewResult;
+
+            Assert.IsTrue(result.Model is ConfirmEmailViewModel);
+            Assert.IsTrue(((ConfirmEmailViewModel)result.Model).Message == "There has been a problem with your registration details. Please try registering again or contact an administrator.");
+        }
+
+        [Test]
+        public async Task ConfirmRegistrationUserCreationFailsResultsInErrorMessage()
+        {
+            _mockRegisterService.Setup(rs => rs.DecodeRegistrationDetails("test-payload"))
+                .Returns(new RegistrationDTO { Email = "test@email.com", LegislativeAreas = new List<string>(), Password = "Password", Reason = "reason", UserRole = Constants.Roles.UKASUser});
+            mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((UKMCABUser)null);
+            mockUserManager.Setup(um => um.CreateAsync(It.IsAny<UKMCABUser>(), It.IsAny<string>())).ReturnsAsync(
+                IdentityResult.Failed(new IdentityError { Code = "User", Description = "Create error" }));
+            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<UKMCABUser>(), Constants.Roles.UKASUser)).ReturnsAsync(
+                IdentityResult.Failed(new IdentityError { Code = "Role", Description = "Create error" }));
+
+            var result = await _sut.ConfirmEmail("test-payload") as ViewResult;
+
+            Assert.IsTrue(result.Model is ConfirmEmailViewModel);
+            Assert.IsTrue(((ConfirmEmailViewModel)result.Model).Message == "There has been a problem registering your account. Please try again or contact an administrator.");
+        }
+
+        [Test]
+        public async Task ConfirmRegistrationRoleLinkingFailsResultsInErrorMessage()
+        {
+            _mockRegisterService.Setup(rs => rs.DecodeRegistrationDetails("test-payload"))
+                .Returns(new RegistrationDTO { Email = "test@email.com", LegislativeAreas = new List<string>(), Password = "Password", Reason = "reason", UserRole = Constants.Roles.UKASUser });
+            mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((UKMCABUser)null);
+            mockUserManager.Setup(um => um.CreateAsync(It.IsAny<UKMCABUser>(), It.IsAny<string>())).ReturnsAsync(
+                IdentityResult.Success);
+            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<UKMCABUser>(), Constants.Roles.UKASUser)).ReturnsAsync(
+                IdentityResult.Failed(new IdentityError { Code = "Role", Description = "Create error" }));
+
+            var result = await _sut.ConfirmEmail("test-payload") as ViewResult;
+
+            Assert.IsTrue(result.Model is ConfirmEmailViewModel);
+            Assert.IsTrue(((ConfirmEmailViewModel)result.Model).Message == "There has been a problem registering your account. Please try again or contact an administrator.");
+        }
+
+        [Test]
+        public async Task ConfirmRegistrationUKASUserResultsInSuccessMessage()
+        {
+            _mockRegisterService.Setup(rs => rs.DecodeRegistrationDetails("test-payload"))
+                .Returns(new RegistrationDTO { Email = "test@email.com", LegislativeAreas = new List<string>(), Password = "Password", Reason = "reason", UserRole = Constants.Roles.UKASUser });
+            mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((UKMCABUser)null);
+            mockUserManager.Setup(um => um.CreateAsync(It.IsAny<UKMCABUser>(), It.IsAny<string>())).ReturnsAsync(
+                IdentityResult.Success);
+            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<UKMCABUser>(), Constants.Roles.UKASUser)).ReturnsAsync(
+                IdentityResult.Success);
+
+            var result = await _sut.ConfirmEmail("test-payload") as ViewResult;
+
+            Assert.IsTrue(result.Model is ConfirmEmailViewModel);
+            Assert.IsTrue(((ConfirmEmailViewModel)result.Model).Message == "You will now be able to login to your account.");
+        }
+
+        [Test]
+        public async Task ConfirmRegistrationNonUKASUserResultsInSuccessMessage()
+        {
+            _mockRegisterService.Setup(rs => rs.DecodeRegistrationDetails("test-payload"))
+                .Returns(new RegistrationDTO { Email = "test@email.com", LegislativeAreas = new List<string>(), Password = "Password", Reason = "reason", UserRole = Constants.Roles.OGDUser });
+            mockUserManager.Setup(um => um.FindByEmailAsync(It.IsAny<string>())).ReturnsAsync((UKMCABUser)null);
+            mockUserManager.Setup(um => um.CreateAsync(It.IsAny<UKMCABUser>(), It.IsAny<string>())).ReturnsAsync(
+                IdentityResult.Success);
+            mockUserManager.Setup(um => um.AddToRoleAsync(It.IsAny<UKMCABUser>(), Constants.Roles.OGDUser)).ReturnsAsync(
+                IdentityResult.Success);
+
+            var result = await _sut.ConfirmEmail("test-payload") as ViewResult;
+
+            mockAsyncNotificationClient.Verify(anc => anc.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(),
+                It.IsAny<Dictionary<string, dynamic>>(), It.IsAny<string>(), It.IsAny<string>()));
+
+            Assert.IsTrue(result.Model is ConfirmEmailViewModel);
+            Assert.IsTrue(((ConfirmEmailViewModel)result.Model).Message == "Your registration request will be reviewed and you will receive notification once approved.");
+        }
     }
 }
