718e2acd8f8605c8daf308d13139a740b3f86733
Andy Rose <andy@cyberian.co.uk>
2023-02-17 14:28:09 +0000

    search desktop view elements in place

---
+++ src/UKMCAB.Web.UI/Areas/Search/Controllers/SearchController.cs
@@ -0,0 +1,40 @@
+﻿using UKMCAB.Web.UI.Models.ViewModels.Search;
+
+namespace UKMCAB.Web.UI.Areas.Search.Controllers
+{
+    [Area("search")]
+    public class SearchController : Controller
+    {
+        [Route("search")]
+        public IActionResult Index(SearchViewModel model)
+        {
+            model = new SearchViewModel
+            {
+                BodyTypeOptions = Constants.Lists.BodyTypes,
+                RegisteredOfficeLocationOptions = Constants.Lists.Countries,
+                TestingLocationOptions = Constants.Lists.Countries,
+                LegislativeAreaOptions = Constants.Lists.LegislativeAreas,
+                SearchResults = FakeResults()
+            };
+            return View(model);
+        }
+        private List<ResultViewModel> FakeResults()
+        {
+            var results = new List<ResultViewModel>();
+            for (int i = 0; i < 20; i++)
+            {
+                results.Add(new ResultViewModel
+                {
+                    Name = "Gateshead MBC (Tyne and Wear Trading Standards)" + i.ToString(),
+                    Address = "Trading Standards, Civic Centre, Regent Street, Gateshead, Tyne & Wear, NE8 1HH",
+                    BodyType = "Approved body and 1 others",
+                    RegisteredOfficeLocation = "United Kingdom",
+                    RegisteredTestLocation = "United Kingdom",
+                    LegislativeArea = "Measuring instructions and 1 others"
+                });
+            }
+
+            return results;
+        }
+    }
+}
