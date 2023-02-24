e34073dddf4f2cc062ecfc4f960f38a592db9c48
Andy Rose <andy@cyberian.co.uk>
2023-02-24 09:47:18 +0000

    Updating clear filters button

---
+++ src/UKMCAB.Web.UI/Extensions/RequestExt.cs
@@ -0,0 +1,18 @@
+﻿using System.Web;
+
+namespace UKMCAB.Web.UI.Extensions
+{
+    public static class RequestExt
+    {
+        public static string RemoveQueryParameters(this HttpRequest request, params string[] queryKeys)
+        {
+            var queryItems = request.QueryString;
+            var collection = HttpUtility.ParseQueryString(queryItems.Value);
+            foreach (var queryKey in queryKeys)
+            {
+                collection.Remove(queryKey);
+            }
+            return request.Path + "?" + collection;
+        }
+    }
+}
