913a5349ae537a065cfba74c4be33e3df7b7f0a2
Andy Rose <andy@cyberian.co.uk>
2023-03-09 11:11:36 +0000

    Amendments to the footer

---
+++ src/UKMCAB.Web.UI/Areas/Home/Controllers/FooterController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace UKMCAB.Web.UI.Areas.Home.Controllers
+{
+    [Area("Home")]
+    public class FooterController : Controller
+    {
+
+        [Route("/privacy-notice")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
