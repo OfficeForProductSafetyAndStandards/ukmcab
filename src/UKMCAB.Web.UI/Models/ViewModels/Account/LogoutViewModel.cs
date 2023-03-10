4bff6692488a0072b97304e2c6b781be9994f406
Andy Rose <andy@cyberian.co.uk>
2023-03-10 14:14:15 +0000

    Changes to sign out process

--- src/UKMCAB.Web.UI/Models/ViewModels/Account/LogoutViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace UKMCAB.Web.UI.Models.ViewModels.Account
-{
-    public class LogoutViewModel: ILayoutModel
-    {
-        public string? Title => "Login";
-        public string ReturnURL { get; set;}
-    }
-}
