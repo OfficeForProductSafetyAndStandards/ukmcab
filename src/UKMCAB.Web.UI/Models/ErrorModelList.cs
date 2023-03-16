038c5277ea9091eb7f6ed7cc5bab4861c0512783
Andy Rose <andy@cyberian.co.uk>
2023-03-16 14:29:22 +0000

    changes to change password validation

--- src/UKMCAB.Web.UI/Models/ErrorModelList.cs
+++ src/UKMCAB.Web.UI/Models/ErrorModelList.cs
@@ -13,6 +13,24 @@ namespace UKMCAB.Web.UI.Models
                 .SelectMany(v => v.Value.Errors.Select(e => new ErrorViewModel { Key = v.Key, Error = e.ErrorMessage }))
                 .ToList();
         }
+
+        public ErrorModelList(ModelStateDictionary modelState, string[] fieldOrder)
+        {
+            ErrorViewModels = modelState
+                .Where(m => m.Value.ValidationState == ModelValidationState.Invalid)
+                .SelectMany(v => v.Value.Errors.Select(e => new ErrorViewModel { Key = v.Key, Error = e.ErrorMessage }))
+                .ToList();
+            if (fieldOrder != null)
+            {
+                var sortedList = ErrorViewModels.Where(e => string.IsNullOrEmpty(e.Key) || fieldOrder.All(f => !f.Equals(e.Key))).ToList();
+                foreach (var field in fieldOrder)
+                {
+                    sortedList.AddRange(ErrorViewModels.Where(e => field.Equals(e.Key)));
+                }
+
+                ErrorViewModels = sortedList;
+            }
+        }
     }
 
     public class ErrorViewModel
