cc492bb3c706fea206594ad5a44ad56a089b4091
kristan66 <kristan@hotmail.co.uk>
2023-04-21 17:10:34 +0100

    Subscriptions UI bits

--- src/UKMCAB.Web.UI/Areas/Subscriptions/Controllers/SubscriptionsController.cs
+++ src/UKMCAB.Web.UI/Areas/Subscriptions/Controllers/SubscriptionsController.cs
@@ -1,4 +1,8 @@
-﻿using UKMCAB.Subscriptions.Core.Services;
+﻿using Microsoft.IdentityModel.Tokens;
+using UKMCAB.Common.Security;
+using UKMCAB.Subscriptions.Core.Domain;
+using UKMCAB.Subscriptions.Core.Services;
+using UKMCAB.Web.UI.Areas.Subscriptions.Models;
 
 namespace UKMCAB.Web.UI.Areas.Subscriptions.Controllers;
 
@@ -10,6 +14,15 @@ public class SubscriptionsController : Controller
 
     public static class Routes
     {
+
+        public const string RequestSearchSubscription = "subscription.request.search";
+        public const string RequestConfirmSubscription = "subscription.request.confirm";
+        public const string RequestSubscriptionFrequency = "subscription.request.frequency";
+        public const string RequestSubscriptionEmailAddress = "subscription.request.emailaddress";
+        public const string RequestSubscription = "subscription.request";
+        public const string RequestedSubscription = "subscription.requested";
+        
+        
         public const string ConfirmSearchSubscription = "subscription.confirm.search";
         public const string ConfirmCabSubscription = "subscription.confirm.cab";
         public const string ConfirmUpdatedEmailAddress = "subscription.confirm.update-email-address";
@@ -22,6 +35,79 @@ public class SubscriptionsController : Controller
     {
         _subscriptions = subscriptions;
     }
+
+    [HttpGet("subscribe/request/search", Name = Routes.RequestSearchSubscription)]
+    public IActionResult RequestSearchSubscription()
+    {
+        var req = new SubscriptionRequestViewModel
+        {
+            SubscriptionType = Models.SubscriptionType.Search,
+            SearchQueryString = Request.QueryString.Value,
+        };
+
+        return RedirectToRoute(Routes.RequestConfirmSubscription, new { tok = JsonBase64UrlToken.Serialize(req) });
+    }
+
+    [Route("subscribe/request/confirm", Name = Routes.RequestConfirmSubscription)]
+    public IActionResult RequestConfirmSubscription(string tok)
+    {
+        if(Request.Method == HttpMethod.Get.Method)
+        {
+            return View();
+        }
+        else
+        {
+            return RedirectToRoute(Routes.RequestSubscriptionFrequency, new { tok });
+        }
+    }
+
+    [Route("subscribe/request/frequency", Name = Routes.RequestSubscriptionFrequency)]
+    public IActionResult RequestSubscriptionFrequency(string tok, [FromForm] string? frequency = null)
+    {
+        if (Request.Method == HttpMethod.Get.Method)
+        {
+            return View();
+        }
+        else
+        {
+            tok = JsonBase64UrlToken.Pipe<SubscriptionRequestViewModel>(tok, x => x.Frequency = Enum.Parse<Frequency>(frequency!));
+            return RedirectToRoute(Routes.RequestSubscriptionEmailAddress, new { tok });
+        }
+    }
+
+    [HttpGet("subscribe/request/email-address", Name = Routes.RequestSubscriptionEmailAddress)]
+    public IActionResult RequestSubscriptionEmailAddress(string tok) => View(tok);
+
+
+    [HttpPost("subscribe/request", Name = Routes.RequestSubscription)]
+    public async Task<IActionResult> RequestSubscriptionAsync(string tok, [FromForm] string? emailAddress)
+    {
+        var req = JsonBase64UrlToken.Deserialize<SubscriptionRequestViewModel>(tok)
+            .Pipe(x => x.EmailAddress = emailAddress)
+            ?? throw new Exception("Token deserialised to null");
+
+        if (req.SubscriptionType == Models.SubscriptionType.Search)
+        {
+            await _subscriptions.RequestSubscriptionAsync(new SearchSubscriptionRequest(req.EmailAddress, req.SearchQueryString, req.Frequency.Value));
+        }
+        else
+        {
+            await _subscriptions.RequestSubscriptionAsync(new CabSubscriptionRequest(emailAddress, req.CabId ?? throw new Exception("CAB id should not be null"), req.Frequency.Value));
+        }
+        return RedirectToRoute(Routes.RequestedSubscription, new { tok = JsonBase64UrlToken.Serialize(req) });
+    }
+
+    [HttpGet("subscribe/requested", Name = Routes.RequestedSubscription)]
+    public IActionResult RequestedSubscription(string tok)
+    {
+        var viewModel = JsonBase64UrlToken.Deserialize<SubscriptionRequestViewModel>(tok);
+        return View(viewModel);
+    }
+
+
+
+
+
 
     [HttpGet("subscribe/search/confirm", Name = Routes.ConfirmSearchSubscription)]
     public async Task<IActionResult> ConfirmSearchSubscriptionAsync(string token)
---
+++ src/UKMCAB.Web.UI/Areas/Subscriptions/Models/SubscriptionRequestViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Text.Json.Serialization;
+
+namespace UKMCAB.Web.UI.Areas.Subscriptions.Models;
+
+public enum SubscriptionType
+{
+    Search,
+    Cab
+}
+
+public class SubscriptionRequestViewModel
+{
+    [JsonPropertyName("st")]
+    public SubscriptionType? SubscriptionType { get; set; }
+
+    [JsonPropertyName("c")]
+    public Guid? CabId { get; set; }
+
+    [JsonPropertyName("q")]
+    public string? SearchQueryString { get; set; }
+
+    [JsonPropertyName("e")]
+    public string? EmailAddress { get; set; }
+
+    [JsonPropertyName("f")]
+    public Frequency? Frequency { get; set; }
+
+
+}
