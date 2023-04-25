9cafbce02c271e605d9a3ba155d7c1782c69f1c8
Andy Rose <andy@cyberian.co.uk>
2023-04-25 10:42:39 +0100

    Adding confirmation page

---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CABConfirmationViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UKMCAB.Web.UI.Models.ViewModels.Admin
+{
+    public class CABConfirmationViewModel : ILayoutModel
+    {
+        public string CABId { get; set; }
+        public string Name { get; set; }
+        public string CABNumber { get; set; }
+        public string? Title => "CAB confirmation";
+    }
+}
