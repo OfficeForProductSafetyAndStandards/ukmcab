3b1d71c74e7182d47173b070bf27fbb0dc4e5217
Andy Rose <andy@cyberian.co.uk>
2023-05-18 13:42:06 +0100

    Adding county field

--- src/UKMCAB.Data/Models/Document.cs
+++ src/UKMCAB.Data/Models/Document.cs
@@ -29,6 +29,7 @@
         public string? AddressLine1 { get; set; }
         public string? AddressLine2 { get; set; }
         public string? TownCity { get; set; }
+        public string? County { get; set; }
         public string? Postcode { get; set; }
         public string Country { get; set; }
         public string Website { get; set; }
