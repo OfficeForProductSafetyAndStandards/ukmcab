58194c2b7c4fe0595f3f3e77246e7c25742646eb
Andy Rose <andy@cyberian.co.uk>
2023-05-23 08:48:07 +0100

    Adding cookie banner

--- src/UKMCAB.Web.UI/Areas/Home/Controllers/FooterController.cs
+++ src/UKMCAB.Web.UI/Areas/Home/Controllers/FooterController.cs
@@ -35,8 +35,32 @@ namespace UKMCAB.Web.UI.Areas.Home.Controllers
                 Title = Constants.PageTitle.CookiesPolicy
             };
             return View(model);
-        }        
-        
+        }
+
+        [Route("/cookies-policy")]
+        [HttpPost]
+        public IActionResult Cookies(string returnURL, string cookies)
+        {
+            if (cookies == "reject")
+            {
+                var cookiesToDelete = Request.Cookies.Keys.Where(c => c.StartsWith("ai_") || c.StartsWith("_ga"));
+                foreach (var cookie in cookiesToDelete)
+                {
+                    Response.Cookies.Delete(cookie);
+                }
+            }
+            Response.Cookies.Append(Constants.AnalyticsOptInCookieName, cookies, new CookieOptions {Expires = DateTimeOffset.UtcNow.AddDays(30)});
+            if (!string.IsNullOrWhiteSpace(returnURL))
+            {
+                return Redirect(returnURL);
+            }
+            var model = new BasicPageModel()
+            {
+                Title = Constants.PageTitle.CookiesPolicy
+            };
+            return View(model);
+        }
+
         [Route("/accessibility-statement")]
         public IActionResult AccessibilityStatement()
         {
