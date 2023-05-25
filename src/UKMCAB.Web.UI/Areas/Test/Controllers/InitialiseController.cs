91514e1aca45684279a12e36b7656b7e3b582af1
Andy Rose <andy@cyberian.co.uk>
2023-05-25 11:55:33 +0100

    Async handling on api initialisation

--- src/UKMCAB.Web.UI/Areas/Test/Controllers/InitialiseController.cs
+++ src/UKMCAB.Web.UI/Areas/Test/Controllers/InitialiseController.cs
@@ -1,5 +1,10 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.ApplicationInsights;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.IdentityModel.Abstractions;
 using UKMCAB.Data;
+using UKMCAB.Infrastructure.Logging;
+using UKMCAB.Infrastructure.Logging.Models;
+using LogEntry = UKMCAB.Infrastructure.Logging.Models.LogEntry;
 
 namespace UKMCAB.Web.UI.Areas.Test.Controllers
 {
@@ -9,29 +14,42 @@ namespace UKMCAB.Web.UI.Areas.Test.Controllers
     public class InitialiseController : Controller
     {
         private readonly IInitialiseDataService _initialiseDataService;
+        private readonly TelemetryClient _temClient;
+        private readonly ILogger<InitialiseController> _logger;
+        private readonly ILoggingService _loggingService;
 
-        public InitialiseController(IInitialiseDataService initialiseDataService)
+        public InitialiseController(IInitialiseDataService initialiseDataService, TelemetryClient temClient, ILogger<InitialiseController> logger, ILoggingService loggingService)
         {
             _initialiseDataService = initialiseDataService;
+            _temClient = temClient;
+            _logger = logger;
+            _loggingService = loggingService;
         }
 
         [HttpPost]
         public async Task<IActionResult> Post()
         {
             var cutOffDate = new DateTime(2023, 5, 27);
-            try
-            {
-                if (DateTime.UtcNow < cutOffDate)
-                {
-                    await _initialiseDataService.InitialiseAsync(true);
-                }
-            }
-            catch (Exception e)
-            {
-                return BadRequest($"Initialisation failed.{e.Message}. {e.StackTrace}");
-            }
 
-            return Ok();
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    if (DateTime.UtcNow < cutOffDate)
+                    {
+                        await _initialiseDataService.InitialiseAsync(true);
+                    }
+                    _temClient.TrackEvent("init_complete");
+                }
+                catch (Exception e)
+                {
+                    _temClient.TrackException(e);
+                    _logger.LogError(e, "Initialisation failed");
+                    _loggingService.Log(new LogEntry(e));
+                }
+            });
+
+            return Accepted();
         }
     }
 }
