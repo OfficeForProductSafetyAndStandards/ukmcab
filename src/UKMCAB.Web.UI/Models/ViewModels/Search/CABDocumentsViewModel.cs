2aeec3984da93eb03a96f62925ff678e13972dac
Andy Rose <andy@cyberian.co.uk>
2023-06-15 14:15:45 +0100

    Adding supporting documents tab to profile page

---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Search/CABDocumentsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using UKMCAB.Data.Models;
+
+namespace UKMCAB.Web.UI.Models.ViewModels.Search
+{
+    public class CABDocumentsViewModel
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string CABId { get; set; }
+        public string DocumentType { get; set; }
+        public List<FileUpload> Documents { get; set; }
+    }
+}
