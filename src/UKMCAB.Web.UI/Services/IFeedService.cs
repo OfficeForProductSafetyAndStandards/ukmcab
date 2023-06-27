cef82293ef85ece152bd20d8fe15a811e3514691
Andy Rose <andy@cyberian.co.uk>
2023-06-27 11:51:15 +0100

    Adding feed to cab profile

--- src/UKMCAB.Web.UI/Services/IFeedService.cs
+++ src/UKMCAB.Web.UI/Services/IFeedService.cs
@@ -1,4 +1,5 @@
 ﻿using System.ServiceModel.Syndication;
+using UKMCAB.Data.Models;
 using UKMCAB.Data.Search.Models;
 
 namespace UKMCAB.Web.UI.Services
@@ -6,5 +7,6 @@ namespace UKMCAB.Web.UI.Services
     public interface IFeedService
     {
         SyndicationFeed GetSyndicationFeed(string feedName, HttpRequest request, IEnumerable<CABIndexItem> items, IUrlHelper url);
+        SyndicationFeed GetSyndicationFeed(string feedName, HttpRequest request, Document item, IUrlHelper url);
     }
 }
