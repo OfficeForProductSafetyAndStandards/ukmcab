0306ebe1eeb23f49cd89470f5a0303c8f7d51e4c
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2023-07-11 20:00:47 +0100

    updated logic and error messages

--- src/UKMCAB.Web.UI/Services/DateService.cs
+++ src/UKMCAB.Web.UI/Services/DateService.cs
@@ -1,81 +1,97 @@
-﻿namespace UKMCAB.Web.UI.Services
+﻿namespace UKMCAB.Web.UI.Services;
+
+public class DateService
 {
-    public class DateService
+    public static bool IsAValidDayOfMonthAndYear(int day, int month, int year)
     {
-        public static bool IsAValidDay(int day, int month, int year)
+        if (IsAValidMonth(month)) 
         {
-            if (IsAValidMonth(month)) 
-            {
-                int maxDay = GetMaxDaysInMonth(month, year);
+            int maxDay = GetMaxDaysInMonth(month, year);
 
-                return day >= 1 && day <= maxDay;
-            }
-            
-            return day <= 31;
-        }        
+            return day >= 1 && day <= maxDay;
+        }
         
-        public static bool IsAValidMonth(int month)
+        return IsAValidDay(day);
+    }        
+    
+    public static bool IsAValidDay(int day)
+    {
+        return day >= 1 && day <= 31;
+    }
+    public static bool IsAValidMonth(int month)
+    {
+        return month >= 1 && month <= 12;          
+
+    }     
+    
+    public static bool IsAValidYear(int year)
+    {
+        return year >= 1;       
+
+    }       
+   
+    public static bool IsFutureDate(int day, int month, int year)
+    {
+        var today = DateTime.Today;
+
+        if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
+            return inputDate > today;
+
+        return false;
+    }
+
+    public static bool IsPastDate(int day, int month, int year)
+    {
+        var today = DateTime.Today;
+
+        if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
+            return inputDate < today;
+
+        return false;
+    }
+
+    public static bool IsWithinFiveYearOfAppointmentDateAndInFuture(int day, int month, int year, DateTime? aptDate)
+    {
+        if(aptDate == null)
         {
-            return month >= 1 && month <= 12;          
-
-        }       
-       
-        public static bool IsTodayOrFuture(int day, int month, int year)
-        {
-            var currentDate = DateTime.Today;
-
-            if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
-                return inputDate >= currentDate;
-
-            return false;
+            return true;
         }
 
-        public static bool IsTodayOrInPast(int day, int month, int year)
+        DateTime appointmentDate = (DateTime)aptDate;            
+        var tomorrow = DateTime.Today.AddDays(1);
+
+        if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
+            return inputDate <= appointmentDate.AddYears(5) && inputDate >= tomorrow;
+
+        return false;
+    }
+    public static bool IsWithinFiveYearAndInThePast(int day, int month, int year)
+    {
+        var tomorrow = DateTime.Today.AddDays(1);
+        var today = DateTime.Today;
+
+        if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
+            return inputDate >= tomorrow.AddYears(-5) && inputDate < today;
+
+        return false;
+    }
+
+    public static int GetMaxDaysInMonth(int month, int year)
+    {
+        if (month == 2)
         {
-            var currentDate = DateTime.Today;
-
-            if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
-                return inputDate <= currentDate;
-
-            return false;
+            return IsLeapYear(year) ? 29 : 28;
+        }
+        else if (month == 4 || month == 6 || month == 9 || month == 11)
+        {
+            return 30;
         }
 
-        public static bool IsWithinFiveYearAndNotInPast(int day, int month, int year, DateTime? aptDate)
-        {
-            DateTime AppointmentDateOrToday = aptDate ?? DateTime.Today;   
+        return 31;
+    }
 
-            if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
-                return inputDate <= AppointmentDateOrToday.AddYears(5) && inputDate >= AppointmentDateOrToday;
-
-            return false;
-        }
-        public static bool IsWithinFiveYearAndNotFuture(int day, int month, int year)
-        {
-            var currentDate = DateTime.Today;
-
-            if (DateTime.TryParse($"{year}/{month}/{day}", out DateTime inputDate))
-                return inputDate >= currentDate.AddYears(-5) && inputDate <= currentDate;
-
-            return false;
-        }
-
-        public static int GetMaxDaysInMonth(int month, int year)
-        {
-            if (month == 2)
-            {
-                return IsLeapYear(year) ? 29 : 28;
-            }
-            else if (month == 4 || month == 6 || month == 9 || month == 11)
-            {
-                return 30;
-            }
-
-            return 31;
-        }
-
-        private static bool IsLeapYear(int year)
-        {
-            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-        }
+    private static bool IsLeapYear(int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
--- src/UKMCAB.Web.UI/Services/DateValidator.cs
+++ src/UKMCAB.Web.UI/Services/DateValidator.cs
@@ -1,156 +1,169 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.EntityFrameworkCore.ChangeTracking;
 
-namespace UKMCAB.Web.UI.Services
+namespace UKMCAB.Web.UI.Services;
+
+public class DateValidator
 {
-    public class DateValidator
+    public static DateTime? CheckDate(ModelStateDictionary modelState, string day, string month, string year, string modelKey, string errorMessagePart, DateTime? aptDate = null)
     {
-        public static DateTime? CheckDate(ModelStateDictionary modelState, string day, string month, string year, string modelKey, string errorMessagePart, DateTime? aptDate = null)
+        var date = $"{day}/{month}/{year}";
+        var dayParsed = int.TryParse(day, out int parsedDay);
+        var monthParsed = int.TryParse(month, out int parsedMonth);
+        var yearParsed = int.TryParse(year, out int parsedYear);
+
+        if (dayParsed && monthParsed && yearParsed)
         {
-            var date = $"{day}/{month}/{year}";
 
-            if (int.TryParse(day, out int dayNum) && int.TryParse(month, out int monthNum) && int.TryParse(year, out int yearNum))
+            if(RealDateErrorsAdded(modelState,modelKey,errorMessagePart,parsedDay, parsedMonth, parsedYear) > 0)
             {
-
-                //if (!DateService.IsAValidDay(dayNum, monthNum, yearNum) || !DateService.IsAValidMonth(monthNum))
-                //{
-                //    int maxDay = DateService.GetMaxDaysInMonth(monthNum, yearNum);
-                //    modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
-
-                //    return null;
-                //}
-
-                var usedModelKey = string.Empty;
-
-                if (!DateService.IsAValidMonth(monthNum))
-                {
-                    usedModelKey = $"{modelKey}Month";
-
-                    modelState.AddModelError($"{modelKey}Month", string.Empty);
-                    //modelState.AddModelError(usedModelKey, $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
-
-                    //return null;
-
-                }
-
-                if (!DateService.IsAValidDay(dayNum, monthNum, yearNum))
-                {
-                    modelState.AddModelError($"{modelKey}Day", string.Empty);
-
-                    //if (string.IsNullOrWhiteSpace(usedModelKey))
-                    //{
-                    //    modelState.AddModelError($"{modelKey}Day", $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
-                    //}
-                    //else
-                    //{
-                    //    modelState.AddModelError($"{modelKey}Day", string.Empty);
-                    //}
-
-                    //return null;
-                }
-
-                if (!DateService.IsAValidDay(dayNum, monthNum, yearNum) || !DateService.IsAValidMonth(monthNum))
-                {
-                    modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
-                    return null;
-                }
-
-                if (!DateService.IsTodayOrInPast(dayNum, monthNum, yearNum) && modelKey == "AppointmentDate")
-                {
-                    AddAllFieldErrors(modelState, modelKey);
-                    modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be in the past.");
-                    return null;
-                }
-
-                if (!DateService.IsTodayOrFuture(dayNum, monthNum, yearNum) && modelKey == "ReviewDate")
-                {
-                    AddAllFieldErrors(modelState, modelKey);
-                    modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be in the future.");
-                    return null;
-                }
-
-                if (!DateService.IsWithinFiveYearAndNotInPast(dayNum, monthNum, yearNum, aptDate) && modelKey == "ReviewDate")
-                {
-                    AddAllFieldErrors(modelState, modelKey);
-                    modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be within 5 years of the appointment date.");
-                    return null;
-                }
-
-                if (DateTime.TryParse(date, out DateTime dateTime))
-                    return dateTime;
-
+                return null;
             }
 
-            if (int.TryParse(day, out int dayNum1) || int.TryParse(month, out int monthNum1) || int.TryParse(year, out int yearNum1))
+            if (!DateService.IsAValidDayOfMonthAndYear(parsedDay, parsedMonth, parsedYear))
             {
-                var missingField = string.Empty;
-                var missingFields = new List<string>();
-                string modelKeyLocal = string.Empty;
-
-              
-
-                if (string.IsNullOrWhiteSpace(day))
-                {
-                    missingField += "day and ";
-                    modelKeyLocal = $"{modelKey}Day";
-                    //modelState.AddModelError(modelKeyLocal, string.Empty);
-                    missingFields.Add(modelKeyLocal);
-                }
-                    
-
-                if (string.IsNullOrWhiteSpace(month))
-                {
-                    missingField += "month and ";
-                    modelKeyLocal = $"{modelKey}Month";
-                    //modelState.AddModelError(modelKeyLocal, string.Empty);
-                    missingFields.Add(modelKeyLocal);
-                }
-                    
-
-                if (string.IsNullOrWhiteSpace(year))
-                {
-                    missingField += "year";
-                    modelKeyLocal = $"{modelKey}Year";
-                    //modelState.AddModelError(modelKeyLocal, string.Empty);
-                    missingFields.Add(modelKeyLocal);
-                }
-                    
-
-                missingField =  missingField.TrimEnd(' ', 'a', 'n', 'd', ' ');
-                //if (!string.IsNullOrEmpty(missingField))
-                //{
-                //    modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must include a {missingField}.");
-                //    return null;
-                //}
-                if(missingFields.Count > 0)
-                {
-                    //int c = 1;
-
-                    foreach (var field in missingFields)
-                    {
-                        modelState.AddModelError(field, string.Empty);
-                    }
-
-                    modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must include a {missingField}.");
-
-                    return null;
-                }
+                AddInvalidDayOfMonthAndYearError(modelState, modelKey, errorMessagePart);
                 
+                return null;
             }
 
-            if (!date.Equals("//"))
+            if (!DateService.IsPastDate(parsedDay, parsedMonth, parsedYear) && modelKey == "AppointmentDate")
             {
-                modelState.AddModelError(modelKey, $"The {errorMessagePart} date is not in a valid date format");
+                AddDateMustBeInPastErrors(modelState, modelKey, errorMessagePart);
+                return null;
             }
 
-            return null;
+            if (!DateService.IsFutureDate(parsedDay, parsedMonth, parsedYear) && modelKey == "ReviewDate")
+            {
+                AddDateMustBeInFutureErrors(modelState, modelKey, errorMessagePart);
+                return null;
+            }
+
+            if (!DateService.IsWithinFiveYearOfAppointmentDateAndInFuture(parsedDay, parsedMonth, parsedYear, aptDate) && modelKey == "ReviewDate")
+            {
+                AddDateMustBeWithin5YearsErrors(modelState, modelKey, errorMessagePart);
+                return null;
+            }
+
+            if (DateTime.TryParse(date, out DateTime dateTime))
+                return dateTime;
+
         }
 
-        private static void AddAllFieldErrors(ModelStateDictionary modelState, string modelKey)
+        if (dayParsed || monthParsed || yearParsed)
         {
-            modelState.AddModelError($"{modelKey}Day", string.Empty);
+            AddMissingDateFieldsErrors(modelState, modelKey, errorMessagePart, day, month, year);
+            return null;
+        }      
+
+        if (!date.Equals("//"))
+        {
+            AddInvalidDateFormatErrors(modelState, modelKey, errorMessagePart, parsedDay, parsedMonth, parsedYear);
+        }            
+       
+        return null;
+    }
+
+    private static void AddInvalidDayOfMonthAndYearError(ModelStateDictionary modelState, string modelKey, string errorMessagePart)
+    {
+        modelState.AddModelError($"{modelKey}Day", string.Empty);
+        modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
+    }
+
+    private static int RealDateErrorsAdded(ModelStateDictionary modelState, string modelKey, string errorMessagePart, int day, int month, int year)
+    {
+        int errorsAdded = 0;
+
+        if (!DateService.IsAValidDay(day))
+        {
+            modelState.AddModelError($"{modelKey}Day", string.Empty);   
+            errorsAdded++;
+        }        
+        
+        if (!DateService.IsAValidMonth(month))
+        {
             modelState.AddModelError($"{modelKey}Month", string.Empty);
+            errorsAdded++;
+        }
+        
+        if (!DateService.IsAValidYear(year))
+        {
+            modelState.AddModelError($"{modelKey}Year", string.Empty);
+            errorsAdded++;
+        }
+
+        if(errorsAdded > 0)
+        {
+            modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must be a real date.");
+        }
+
+        return errorsAdded;
+    }
+
+    private static void AddMissingDateFieldsErrors(ModelStateDictionary modelState, string modelKey, string errorMessagePart, string day, string month, string year)
+    {
+        var missingField = string.Empty;
+
+        if (string.IsNullOrWhiteSpace(day))
+        {
+            missingField += "day and ";
+            modelState.AddModelError($"{modelKey}Day", string.Empty);
+        }
+
+        if (string.IsNullOrWhiteSpace(month))
+        {
+            missingField += "month and ";
+            modelState.AddModelError($"{modelKey}Month", string.Empty);
+        }
+
+        if (string.IsNullOrWhiteSpace(year))
+        {
+            missingField += "year";
             modelState.AddModelError($"{modelKey}Year", string.Empty);
         }
+
+        missingField = missingField.TrimEnd(' ', 'a', 'n', 'd', ' ');
+
+        modelState.AddModelError(modelKey, $"{errorMessagePart.ToSentenceCase()} date must include a {missingField}.");
+    }
+
+    private static void AddInvalidDateFormatErrors(ModelStateDictionary modelState, string modelKey, string errorMessagePart, int parsedDay, int parsedMonth, int parsedYear)
+    {
+            
+        if (parsedDay == 0)
+            modelState.AddModelError($"{modelKey}Day", string.Empty);
+            
+        if (parsedMonth == 0)
+            modelState.AddModelError($"{modelKey}Month", string.Empty);
+            
+        if (parsedYear == 0)
+            modelState.AddModelError($"{modelKey}Year", string.Empty);
+
+           
+        modelState.AddModelError(modelKey, $"The {errorMessagePart} date is not in a valid date format");
+    }
+    private static void AddAllFieldErrors(ModelStateDictionary modelState, string modelKey)
+    {
+        modelState.AddModelError($"{modelKey}Day", string.Empty);
+        modelState.AddModelError($"{modelKey}Month", string.Empty);
+        modelState.AddModelError($"{modelKey}Year", string.Empty);
+    }
+
+    private static void AddDateMustBeInFutureErrors(ModelStateDictionary modelState, string modelKey, string errorMessagePart)
+    {
+        AddAllFieldErrors(modelState, modelKey);
+        modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be in the future.");
+    }
+
+    private static void AddDateMustBeInPastErrors(ModelStateDictionary modelState, string modelKey, string errorMessagePart)
+    {
+        AddAllFieldErrors(modelState, modelKey);
+        modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be in the past.");
+    }
+
+    private static void AddDateMustBeWithin5YearsErrors(ModelStateDictionary modelState, string modelKey, string errorMessagePart)
+    {
+        AddAllFieldErrors(modelState, modelKey);
+        modelState.AddModelError(modelKey, $"The {errorMessagePart} date must be within 5 years of the appointment date.");
     }
 }
