04d7ef4de00d7313fda5dfa06f80b9233a052c62
Andy Rose <andy@cyberian.co.uk>
2023-07-28 12:01:33 +0100

    Adding pending user account list

---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/User/UserAccountRequestListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using UKMCAB.Data.Models.Users;
+
+namespace UKMCAB.Web.UI.Models.ViewModels.Admin.User
+{
+    public class UserAccountRequestListViewModel : ILayoutModel
+    {
+        public List<UserAccountRequest> UserAccountRequests { get; set; }
+        public string? Title => "Pending user account requests";
+    }
+}
