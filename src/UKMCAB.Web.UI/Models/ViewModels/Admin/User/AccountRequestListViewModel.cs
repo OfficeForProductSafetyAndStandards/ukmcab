4959feb7733453311b795ab5fa9fa394d803493a
Andy Rose <andy@cyberian.co.uk>
2023-07-28 14:06:06 +0100

    ukmcab-1053 pending accounts list refactor (#238)
    * Adding pending user account list
    
    * Refactor and adding pending accounts link to user list page

---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/User/AccountRequestListViewModel.cs
@@ -0,0 +1,10 @@
+﻿using UKMCAB.Data.Models.Users;
+
+namespace UKMCAB.Web.UI.Models.ViewModels.Admin.User
+{
+    public class AccountRequestListViewModel : ILayoutModel
+    {
+        public List<UserAccountRequest> UserAccountRequests { get; set; }
+        public string? Title => "Pending user account requests";
+    }
+}
