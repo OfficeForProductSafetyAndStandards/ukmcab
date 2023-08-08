97def05ef565f0627bf97319be6a44ba735c9da3
Andy Rose <andy@cyberian.co.uk>
2023-08-08 11:14:28 +0100

    Hotfix for search filters and random sort generator

--- src/UKMCAB.Web.UI/Services/RandomSortGenerator.cs
+++ src/UKMCAB.Web.UI/Services/RandomSortGenerator.cs
@@ -1,36 +1,72 @@
 ﻿using Azure;
 using Azure.Search.Documents.Indexes;
+using Microsoft.ApplicationInsights;
 using UKMCAB.Common.ConnectionStrings;
 using UKMCAB.Data;
 using UKMCAB.Data.CosmosDb.Services;
 using UKMCAB.Data.Models;
+using UKMCAB.Infrastructure.Cache;
+using UKMCAB.Infrastructure.Logging;
+using UKMCAB.Infrastructure.Logging.Models;
 
 namespace UKMCAB.Web.UI.Services
 {
     public class RandomSortGenerator : BackgroundService
     {
-        private Timer _timer;
         private ICABRepository _repository;
+        private readonly TelemetryClient _telemetryClient;
+        private readonly ILoggingService _loggingService;
+        private readonly IDistCache _distCache;
         private SearchIndexerClient _searchIndexerClient;
 
-        public RandomSortGenerator(ICABRepository cabRepository, CognitiveSearchConnectionString connectionString)
+        public RandomSortGenerator(ICABRepository cabRepository, CognitiveSearchConnectionString connectionString, TelemetryClient telemetryClient, ILoggingService loggingService, IDistCache distCache)
         {
             _repository = cabRepository;
+            _telemetryClient = telemetryClient;
+            _loggingService = loggingService;
+            _distCache = distCache;
             _searchIndexerClient = new SearchIndexerClient(new Uri(connectionString.Endpoint),
                 new AzureKeyCredential(connectionString.ApiKey));
 
         }
-        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            var now = DateTime.UtcNow;
-            var tomorrow = now.AddDays(1);
-            var schedule = new DateTime(tomorrow.Year, tomorrow.Month, tomorrow.Day, 3, 0, 0);
-            var difference = schedule - now;
-            _timer = new Timer(RegenerateRandomSortValues, null, difference, TimeSpan.FromDays(1));
-            return Task.CompletedTask;
+            var nextSchedulesRun = DateTime.UtcNow.Hour < 3 ? DateTime.UtcNow.Date.Add(new TimeSpan(3, 0, 0)) : DateTime.UtcNow.Date.Add(new TimeSpan(1, 3, 0, 0));
+            var lockName = StringExt.Keyify(nameof(RandomSortGenerator), nameof(ExecuteAsync));
+            var lockOwner = LockOwner.Create();
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                if (DateTime.UtcNow > nextSchedulesRun)
+                {
+                    try
+                    {
+                        var got = await _distCache.LockTakeAsync(lockName, lockOwner, TimeSpan.FromMinutes(1));
+                        if (got)
+                        {
+                            await RegenerateRandomSortValues();
+                            _telemetryClient.TrackEvent(AiTracking.Events.RandomSortGeneratorRun, new Dictionary<string, string> { { AiTracking.Metrics.RandomSortGeneratorRunDateTime, nextSchedulesRun.ToLongDateString() } });
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        _telemetryClient.TrackException(ex);
+                        _loggingService.Log(new LogEntry(ex));
+                    }
+                    finally
+                    {
+                        await _distCache.LockReleaseAsync(lockName, lockOwner);
+                        nextSchedulesRun = nextSchedulesRun.AddDays(1);
+                    }
+                }
+                else
+                {
+                    await Task.Delay(new TimeSpan(0, 0, 1), stoppingToken);
+                }
+            }
         }
 
-        private async void RegenerateRandomSortValues(object? state)
+        private async Task RegenerateRandomSortValues()
         {
             var allCabs = await _repository.Query<Document>(d => d.StatusValue == Status.Published);
             foreach (var cab in allCabs)
