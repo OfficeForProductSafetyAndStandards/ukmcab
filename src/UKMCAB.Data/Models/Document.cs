58341a76b4b4b14d4446b68a37c478ec6d334b1b
Andy Rose <andy@cyberian.co.uk>
2023-09-05 15:26:14 +0100

    Minor change to aid back compatibility

--- src/UKMCAB.Data/Models/Document.cs
+++ src/UKMCAB.Data/Models/Document.cs
@@ -12,8 +12,28 @@ namespace UKMCAB.Data.Models
         // Audit
         // Legacy audit
         public Audit Created { get; set; }
-        public Audit LastUpdated { get; set; }
-        // Used by the search index, saves a lot of effort to flatten the model in the data source
+
+        private Audit lastUpdated;
+        public Audit LastUpdated
+        {
+            get
+            {
+                if (lastUpdated != null)
+                {
+                    return lastUpdated;
+                }
+                else if (AuditLog != null && AuditLog.Any())
+                {
+                    return AuditLog.Last();
+                }
+                else
+                {
+                    return default(Audit);
+                }
+            }
+            set => lastUpdated = value;
+        }
+
         public Audit Published { get; set; }
         public Audit Archived { get; set; }
         public string ArchivedReason { get; set; }
