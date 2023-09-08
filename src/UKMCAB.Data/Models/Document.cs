da1fe5b5c490e3c5ca89ca49d540d374280ff1fa
Andy Rose <andy@cyberian.co.uk>
2023-09-08 14:38:46 +0100

    New user group filter field

--- src/UKMCAB.Data/Models/Document.cs
+++ src/UKMCAB.Data/Models/Document.cs
@@ -9,40 +9,11 @@ namespace UKMCAB.Data.Models
         public Status StatusValue { get; set; }
         public string Status => StatusValue.ToString();
 
-        // Audit
-        // Legacy audit
-        // This is to support back compatibility during the 2.0 release, all legacy audit fields can ben removed in future release
-        public Audit Created { get; set; }
-
-        private Audit lastUpdated;
-        public Audit LastUpdated
-        {
-            get
-            {
-                if (lastUpdated != null)
-                {
-                    return lastUpdated;
-                }
-                else if (AuditLog != null && AuditLog.Any())
-                {
-                    return AuditLog.Last();
-                }
-                else
-                {
-                    return default(Audit);
-                }
-            }
-            set => lastUpdated = value;
-        }
-
-        public Audit Published { get; set; }
-        public Audit Archived { get; set; }
-        public string ArchivedReason { get; set; }
         // New audit
         public List<Audit> AuditLog { get; set; }
         // Used by the search index, saves a lot of effort to flatten the model in the data source
         public DateTime LastUpdatedDate => AuditLog != null && AuditLog.Any() ? AuditLog.Max(al => al.DateTime) : DateTime.MinValue;
-    
+        public string LastUserGroup => AuditLog != null && AuditLog.Any() ? AuditLog.OrderBy(al => al.DateTime).Last().UserRole : string.Empty;
 
         // About
         public string CABId { get; set; }
