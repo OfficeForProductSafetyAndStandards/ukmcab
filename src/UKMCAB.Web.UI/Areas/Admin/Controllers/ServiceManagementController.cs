4d16fb222dcf211e0a76264b33678c7e104af579
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2023-10-10 18:24:10 +0100

    initial commit

---
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/ServiceManagementController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UKMCAB.Web.UI.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    //[Area("admin"), Authorize]
+    public class ServiceManagementController : Controller
+    {
+        public static class Routes
+        {
+            public const string ServiceManagement = "service.management";
+        }
+
+        [HttpGet("admin/service-management", Name = "service.management")]
+        public IActionResult ServiceManagement()
+        {
+            return View();
+        }
+    }
+}
