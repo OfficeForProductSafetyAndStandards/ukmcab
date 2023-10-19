31838534a1bfc1441d5cd2b8ade7b714e53732a9
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2023-10-19 17:19:27 +0100

    Update CABRepository.cs

--- src/UKMCAB.Data/CosmosDb/Services/CABRepository.cs
+++ src/UKMCAB.Data/CosmosDb/Services/CABRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Azure.Cosmos.Linq;
 using System.Linq.Expressions;
 using System.Net;
+using UKMCAB.Common;
 using UKMCAB.Common.ConnectionStrings;
 using UKMCAB.Data.Models;
 
@@ -24,54 +25,14 @@ namespace UKMCAB.Data.CosmosDb.Services
             var database = client.GetDatabase(DataConstants.CosmosDb.Database);
             _container = database.GetContainer(DataConstants.CosmosDb.Container);
             var items = await Query<Document>(_container, document => true);
-            foreach (var legacyDocument in items)
+
+            if (items.Any() && items.Any(doc => !doc.Version?.Equals(DataConstants.Version.Number) ?? true))
             {
-                if (legacyDocument.StatusValue == Status.Created)
+                foreach (var document in items)
                 {
-                    await Delete(legacyDocument);
+                    document.Version = DataConstants.Version.Number;
+                    await UpdateAsync(document);
                 }
-            }
-
-            items = await Query<Document>(_container, document => true);
-            
-            foreach (var document in items)
-            {
-                if (document.AuditLog.Any())
-                {
-                    continue;
-                }
-                var auditLog = new List<Audit>();
-                if (document.Created != null)
-                {
-                    var audit = document.Created;
-                    auditLog.Add(new Audit(audit.UserId, audit.UserName, audit.UserRole ?? "opss", audit.DateTime,
-                        AuditActions.Created));
-                }
-
-                if (document.LastUpdated != null && document.StatusValue == Status.Draft)
-                {
-                    var audit = document.LastUpdated;
-                    auditLog.Add(new Audit(audit.UserId, audit.UserName, audit.UserRole ?? "opss", audit.DateTime,
-                        AuditActions.Saved));
-                }
-
-                if (document.Published != null)
-                {
-                    var audit = document.Published;
-                    auditLog.Add(new Audit(audit.UserId, audit.UserName, audit.UserRole ?? "opss", audit.DateTime,
-                        AuditActions.Published));
-                }
-
-                if (document.Archived != null)
-                {
-                    var audit = document.Archived;
-                    auditLog.Add(new Audit(audit.UserId, audit.UserName, audit.UserRole ?? "opss", audit.DateTime,
-                        AuditActions.Archived, document.ArchivedReason));
-                }
-
-                document.AuditLog = auditLog.OrderBy(al => al.DateTime).ToList();
-
-                await Update(document);
             }
 
             return force;
@@ -85,7 +46,6 @@ namespace UKMCAB.Data.CosmosDb.Services
             {
                 return response.Resource;
             }
-
             return null;
         }
 
@@ -109,10 +69,17 @@ namespace UKMCAB.Data.CosmosDb.Services
             return list;
         }
 
+        [Obsolete("Use " + nameof(UpdateAsync))]
         public async Task<bool> Update(Document document)
         {
             var response = await _container.UpsertItemAsync(document);
             return response.StatusCode == HttpStatusCode.OK;
+        }
+
+        public async Task UpdateAsync(Document document)
+        {
+            var response = await _container.UpsertItemAsync(document);
+            Guard.IsTrue(response.StatusCode == HttpStatusCode.OK, $"The CAB document was not updated; http status={response.StatusCode}");
         }
 
         public async Task<bool> Delete(Document document)
