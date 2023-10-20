d13e88746c59d32a3530adf3238edd1546716704
acarbutt <30597375+acarbutt@users.noreply.github.com>
2023-10-20 10:59:30 +0100

    Create unit tests projects (#328)
    * Create unit tests projects

---
+++ src/UKMCAB.Common.Tests/EfficiencyTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace UKMCAB.Common.Tests;
+
+[TestFixture]
+public class EfficiencyTest
+{
+    [Test]
+    public async Task TestDoOnceAsync()
+    {
+        var i = 0;
+        await Efficiency.DoOnceAsync("t11", "t21", () => { i++; return Task.CompletedTask; });
+        await Efficiency.DoOnceAsync("t11", "t21", () => { i++; return Task.CompletedTask; });
+        Assert.That(i, Is.EqualTo(1)); 
+        await Efficiency.DoOnceAsync("t11", "t31", () => { i++; return Task.CompletedTask; });
+        Assert.That(i, Is.EqualTo(2));
+    }
+
+    [Test]
+    public void TestDoOnce()
+    {
+        var i = 0;
+        Efficiency.DoOnce("t1", "t2", () => i++);
+        Efficiency.DoOnce("t1", "t2", () => i++);
+        Assert.That(i, Is.EqualTo(1));
+        Efficiency.DoOnce("t1", "t3", () => i++);
+        Assert.That(i, Is.EqualTo(2));
+    }
+}
