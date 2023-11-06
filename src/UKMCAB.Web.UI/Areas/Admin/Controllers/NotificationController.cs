d656f5eb6e23f185b9f10e9163b95f01c2e2e210
Gopi <gopi.govind@olivejar.co.uk>
2023-11-06 16:20:16 +0000

    split controllers

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/NotificationController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/NotificationController.cs
@@ -1,12 +1,9 @@
 using System.Globalization;
 using UKMCAB.Core.Services.Users;
 using Microsoft.AspNetCore.Authorization;
-using UKMCAB.Core.Domain.Workflow;
 using UKMCAB.Core.Security;
 using UKMCAB.Core.Services.CAB;
 using UKMCAB.Core.Services.Workflow;
-using UKMCAB.Data.Domain;
-using UKMCAB.Web.UI.Areas.Search.Controllers;
 using UKMCAB.Web.UI.Models.ViewModels.Admin.Notification;
 using UKMCAB.Web.UI.Models.ViewModels.Shared;
 
@@ -21,7 +18,6 @@ public class NotificationController : Controller
         public const string NotificationsAssignedToMe = "admin.notifications.assigned.me";
         public const string NotificationsAssignedToMyGroup = "admin.notifications.assigned.group";
         public const string NotificationsCompleted = "admin.notifications.completed";
-        public const string NotificationDetails = "admin.notification.details";
     }
 
     private readonly IWorkflowTaskService _workflowTaskService;
@@ -50,7 +46,7 @@ public class NotificationController : Controller
             var cab = await _cabAdminService.GetLatestDocumentAsync(notification.CABId.ToString());
             var item = (From: notification.Submitter.FirstAndLastName, Subject: notification.TaskType.ToString(),
                 CABName: cab.Name, SentOn: notification.SentOn.ToString(CultureInfo.CurrentCulture),
-                CABLink: Url.RouteUrl(Routes.NotificationDetails, notification.Id));
+                CABLink: Url.RouteUrl(NotificationDetailsController.Routes.NotificationDetails, notification.Id));
             items.Add(item);
         }
 
@@ -75,90 +71,5 @@ public class NotificationController : Controller
                     new("From", "From")
                 }));
         return View(model);
-    }
-
-    [HttpGet("details/{id}", Name = Routes.NotificationDetails)]
-    public async Task<IActionResult> Detail(string id)
-    {
-        var workFlowTask = await _workflowTaskService.GetAsync(Guid.Parse(id));
-        var vm = new NotificationDetailViewModel()
-        {
-            NotificationTitle = "Notification Details", //TODO : check with BA
-            Status = WorkFlowTaskStatus(workFlowTask.Completed, workFlowTask.Assignee),
-            From = workFlowTask.Submitter.FirstName + " " +
-                   workFlowTask.Submitter.Surname, //TODO : take latest property
-            Subject = workFlowTask.TaskType.ToString(), //TODO : Bring the enum description rather than enum
-            Reason = workFlowTask.Reason,
-            SentOn = workFlowTask.SentOn
-                .ToShortDateString(), //TODO: create helper for UK  DD/MM/YYYY 14:15 -- verify the profile page -- take Constants
-            CompletedOn = "todo", // "commpleted on not found", //TODO : check with date -- need to take the latest code
-            LastUpdated = workFlowTask.LastUpdatedOn.ToShortDateString(),
-            ViewLink = ("view cab",
-                Url.RouteUrl(CABProfileController.Routes.CabDetails,
-                    workFlowTask.CABId)),
-            CompletedBy = "completed by",
-            AssignedOn =
-                workFlowTask.Assigned != null ? workFlowTask?.Assigned.Value.ToShortDateString() : string.Empty,
-            SelectAssignee = await GetUser(),
-            SelectedAssignee = workFlowTask.Assignee?.FirstName! + " " + workFlowTask.Assignee?.Surname,
-            SelectedAssigneeId = workFlowTask.Assignee?.UserId,
-            UserGroup = "BPSS"
-        };
-
-        return View(vm);
-    }
-
-//todo : Post needs to be implement
-    [HttpPost("details/{id}", Name = Routes.NotificationDetails)]
-    public async Task<IActionResult> Detail(string id, NotificationDetailViewModel model)
-    {
-        var workFlowTask = await _workflowTaskService.GetAsync(Guid.Parse(id));
-
-        model.NotificationTitle = "Notification Details";
-        model.Status = WorkFlowTaskStatus(workFlowTask.Completed, workFlowTask.Assignee);
-        model.From = workFlowTask.Submitter.FirstName + " " + workFlowTask.Submitter.Surname;
-        model.Subject = workFlowTask.TaskType.ToString();
-        model.Reason = workFlowTask.Reason;
-        model.SentOn = workFlowTask.SentOn.ToShortDateString(); //TODO: create helper for UK
-        model.CompletedOn = "todo"; //TODO : check with date
-        model.LastUpdated = workFlowTask.LastUpdatedOn.ToShortDateString();
-        model.ViewLink = ("view cab", "/");
-        model.CompletedBy = "completed by";
-        model.AssignedOn = workFlowTask.Assigned != null
-            ? workFlowTask.Assigned.Value.ToShortDateString()
-            : string.Empty;
-        model.SelectAssignee = await GetUser();
-        model.SelectedAssignee = model.SelectedAssignee;
-        model.UserGroup = "BPSS";
-
-        if (!ModelState.IsValid)
-        {
-            return View(model);
-        }
-        
-        var userAccount = await _userService.GetAsync(model.SelectedAssignee);
-
-        workFlowTask.Assignee =    new User(model.SelectedAssignee, userAccount.FirstName, userAccount.Surname, userAccount.Role);
-        workFlowTask.Assigned = DateTime.Now;
-        await _workflowTaskService.UpdateAsync(workFlowTask);
-
-        return RedirectToRoute(Routes.NotificationsHome);
-    }
-
-    private async Task<List<(string, string)>> GetUser()
-    {
-        var options =
-            new UserAccountListOptions(SkipTake.FromPage(-1, 40), new SortBy("firstName", null), false, null, null);
-        var role = User.IsInRole(Roles.OPSS.Id) ? Roles.OPSS.Id : Roles.UKAS.Id;
-        var users = await _userService.ListAsync(options);
-        var assignees = users.Where(x => x.Role == role)
-            .Select(user => new ValueTuple<string, string>(user.Id, user.FirstName! + " " + user.Surname)).ToList();
-        return assignees;
-    }
-
-    private string WorkFlowTaskStatus(bool isCompleted, User? assignee)
-    {
-        return isCompleted ? "Completed" :
-            assignee == null ? "Unassigned" : "Assigned";
     }
 }
