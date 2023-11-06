4edabb799c84a7940e00cfa5dd439d11750ceedc
Gopi <gopi.govind@olivejar.co.uk>
2023-11-06 13:18:05 +0000

    Service integration

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/NotificationController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/NotificationController.cs
@@ -1,11 +1,12 @@
 using System.Globalization;
-using System.Security.Claims;
+using UKMCAB.Core.Services.Users;
 using Microsoft.AspNetCore.Authorization;
 using UKMCAB.Core.Domain.Workflow;
 using UKMCAB.Core.Security;
 using UKMCAB.Core.Services.CAB;
 using UKMCAB.Core.Services.Workflow;
-using UKMCAB.Subscriptions.Core.Integration.CabService;
+using UKMCAB.Data.Domain;
+using UKMCAB.Web.UI.Areas.Search.Controllers;
 using UKMCAB.Web.UI.Models.ViewModels.Admin.Notification;
 using UKMCAB.Web.UI.Models.ViewModels.Shared;
 
@@ -25,11 +26,16 @@ public class NotificationController : Controller
 
     private readonly IWorkflowTaskService _workflowTaskService;
     private readonly ICABAdminService _cabAdminService;
+    private readonly IUserService _userService;
 
-    public NotificationController(IWorkflowTaskService workflowTaskService, ICABAdminService cabAdminService)
+    public NotificationController(IWorkflowTaskService workflowTaskService,
+        ICABAdminService cabAdminService,
+        IUserService userService
+    )
     {
         _workflowTaskService = workflowTaskService;
         _cabAdminService = cabAdminService;
+        _userService = userService;
     }
 
 
@@ -47,7 +53,7 @@ public class NotificationController : Controller
                 CABLink: Url.RouteUrl(Routes.NotificationDetails, notification.Id));
             items.Add(item);
         }
-        
+
         var model = new NotificationsViewModel
         (
             Constants.PageTitle.Notifications,
@@ -72,45 +78,87 @@ public class NotificationController : Controller
     }
 
     [HttpGet("details/{id}", Name = Routes.NotificationDetails)]
-    public IActionResult Detail(string id)
+    public async Task<IActionResult> Detail(string id)
     {
-        //todo connect to service
-        var status = "Assigned"; // Unassigned,  Assigned, Completed 
-        var assignees = new List<(string Value, string Text)>
+        var workFlowTask = await _workflowTaskService.GetAsync(Guid.Parse(id));
+        var vm = new NotificationDetailViewModel()
         {
-            ("user1", "Test User 1"),
-            ("user2", "Test User 2")
+            NotificationTitle = "Notification Details", //TODO : check with BA
+            Status = WorkFlowTaskStatus(workFlowTask.Completed, workFlowTask.Assignee),
+            From = workFlowTask.Submitter.FirstName + " " +
+                   workFlowTask.Submitter.Surname, //TODO : take latest property
+            Subject = workFlowTask.TaskType.ToString(), //TODO : Bring the enum description rather than enum
+            Reason = workFlowTask.Reason,
+            SentOn = workFlowTask.SentOn
+                .ToShortDateString(), //TODO: create helper for UK  DD/MM/YYYY 14:15 -- verify the profile page -- take Constants
+            CompletedOn = "todo", // "commpleted on not found", //TODO : check with date -- need to take the latest code
+            LastUpdated = workFlowTask.LastUpdatedOn.ToShortDateString(),
+            ViewLink = ("view cab",
+                Url.RouteUrl(CABProfileController.Routes.CabDetails,
+                    workFlowTask.CABId)),
+            CompletedBy = "completed by",
+            AssignedOn =
+                workFlowTask.Assigned != null ? workFlowTask?.Assigned.Value.ToShortDateString() : string.Empty,
+            SelectAssignee = await GetUser(),
+            SelectedAssignee = workFlowTask.Assignee?.FirstName! + " " + workFlowTask.Assignee?.Surname,
+            SelectedAssigneeId = workFlowTask.Assignee?.UserId,
+            UserGroup = "BPSS"
         };
 
-
-        var vm = new NotificationDetailViewModel(
-            "Notification Details",
-            "Notification: Test Notification",
-            "1",
-            Status: status,
-            "From value",
-            "Subject value",
-            "reason value",
-            "11/10/2023 12:15",
-            "12/10/2023 13:00",
-            "23/10/2023 15:00",
-            ("view cab", "/"),
-            "Mr BPSS",
-            "12/10/2023 11:00",
-            assignees,
-            assignees.First().Value, "BPSS"
-        );
         return View(vm);
     }
 
-    //todo : Post needs to be implement
+//todo : Post needs to be implement
     [HttpPost("details/{id}", Name = Routes.NotificationDetails)]
     public async Task<IActionResult> Detail(string id, NotificationDetailViewModel model)
     {
-        if (ModelState.IsValid)
-        {
-        }
+        var workFlowTask = await _workflowTaskService.GetAsync(Guid.Parse(id));
 
-        return View(model);
+        model.NotificationTitle = "Notification Details";
+        model.Status = WorkFlowTaskStatus(workFlowTask.Completed, workFlowTask.Assignee);
+        model.From = workFlowTask.Submitter.FirstName + " " + workFlowTask.Submitter.Surname;
+        model.Subject = workFlowTask.TaskType.ToString();
+        model.Reason = workFlowTask.Reason;
+        model.SentOn = workFlowTask.SentOn.ToShortDateString(); //TODO: create helper for UK
+        model.CompletedOn = "todo"; //TODO : check with date
+        model.LastUpdated = workFlowTask.LastUpdatedOn.ToShortDateString();
+        model.ViewLink = ("view cab", "/");
+        model.CompletedBy = "completed by";
+        model.AssignedOn = workFlowTask.Assigned != null
+            ? workFlowTask.Assigned.Value.ToShortDateString()
+            : string.Empty;
+        model.SelectAssignee = await GetUser();
+        model.SelectedAssignee = model.SelectedAssignee;
+        model.UserGroup = "BPSS";
+
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+        
+        var userAccount = await _userService.GetAsync(model.SelectedAssignee);
+
+        workFlowTask.Assignee =    new User(model.SelectedAssignee, userAccount.FirstName, userAccount.Surname, userAccount.Role);
+        workFlowTask.Assigned = DateTime.Now;
+        await _workflowTaskService.UpdateAsync(workFlowTask);
+
+        return RedirectToRoute(Routes.NotificationsHome);
+    }
+
+    private async Task<List<(string, string)>> GetUser()
+    {
+        var options =
+            new UserAccountListOptions(SkipTake.FromPage(-1, 40), new SortBy("firstName", null), false, null, null);
+        var role = User.IsInRole(Roles.OPSS.Id) ? Roles.OPSS.Id : Roles.UKAS.Id;
+        var users = await _userService.ListAsync(options);
+        var assignees = users.Where(x => x.Role == role)
+            .Select(user => new ValueTuple<string, string>(user.Id, user.FirstName! + " " + user.Surname)).ToList();
+        return assignees;
+    }
+
+    private string WorkFlowTaskStatus(bool isCompleted, User? assignee)
+    {
+        return isCompleted ? "Completed" :
+            assignee == null ? "Unassigned" : "Assigned";
     }
 }
