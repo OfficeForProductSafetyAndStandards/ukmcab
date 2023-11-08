0905d4bc28e5e179a2203bdbe229b10c7ebcc141
Gopi <gopi.govind@olivejar.co.uk>
2023-11-08 11:22:48 +0000

    Enum updated

--- src/UKMCAB.Core/Domain/Workflow/TaskType.cs
+++ src/UKMCAB.Core/Domain/Workflow/TaskType.cs
@@ -12,14 +12,9 @@ public enum TaskType
     ReviewCAB,
     [Description("Approve account request")]
     UserAccountRequest,
-    [Description("Back to UKAS with CAB Published - (completed task)")]
     CABPublished,
-    [Description("Back to UKAS with CAB Declined - (completed task)")]
     CABDeclined,
-    [Description("Approved Unarchive to UKAS (Completed)")]
     UnarchiveApproved,
-    [Description("Declined Unarchive to UKAS (Completed)")]
     UnarchiveDeclined,
-    [Description("User account request approval back to UKAS/OPSS user(Completed)")]
     UserAccountApproved
 }
