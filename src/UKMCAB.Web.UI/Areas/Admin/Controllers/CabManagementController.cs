a6c5a51f5c5365094f5ce1c89147c5634d57d062
acarbutt <30597375+acarbutt@users.noreply.github.com>
2023-11-20 13:16:20 +0000

    Story/ukmcab 750 approve decline initial work in progress (#382)
    * Initial approve cab page and rename cab management controller

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/CabManagementController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/CabManagementController.cs
@@ -8,17 +8,16 @@ using UKMCAB.Web.UI.Models.ViewModels.Shared;
 namespace UKMCAB.Web.UI.Areas.Admin.Controllers
 {
     [Area("admin"), Route("admin"), Authorize(Policy = Policies.CabManagement)]
-    public class AdminController : Controller
+    public class CabManagementController : Controller
     {
         private readonly ICABAdminService _cabAdminService;
 
         public static class Routes
         {
-            public const string Index = "admin.index";
             public const string CABManagement = "admin.cab-management";
         }
 
-        public AdminController(ICABAdminService cabAdminService)
+        public CabManagementController(ICABAdminService cabAdminService)
         {
             _cabAdminService = cabAdminService;
         }
