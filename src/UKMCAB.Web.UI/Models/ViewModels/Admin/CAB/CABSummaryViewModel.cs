ca3fd6bc9d711aaeb31838edb967017cdd15f914
acarbutt <30597375+acarbutt@users.noreply.github.com>
2024-01-02 14:00:16 +0000

    Story/ukmcab 1426 CAB edit lock (#440)

--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABSummaryViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABSummaryViewModel.cs
@@ -13,14 +13,15 @@ namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB
         public List<FileUpload>? Schedules { get; set; }
         public List<FileUpload>? Documents { get; set; }
         public bool ValidCAB { get; set; }
-        public bool ShowError { get; set; }
         public string Title { get; set; } = string.Empty;
         public string TitleHint { get; set; } = string.Empty;
         public string? ReturnUrl { get; set; }
         public bool CABNameAlreadyExists { get; set; }
         public bool CanPublish { get; set; }
         public bool CanSubmitForApproval { get; set; }
-        public bool CanEdit { get; set; }
+        public bool ShowEditActions { get; set; }
         public bool IsOPSSOrInCreatorUserGroup { get; set; }
-}
+        public bool IsEditLocked { get; set; }
+        
+    }
 }
