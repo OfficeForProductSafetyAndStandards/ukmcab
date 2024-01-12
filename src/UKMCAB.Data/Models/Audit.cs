0d507ad52e58472047a2fe9b74870c4f85bf6d84
acarbutt <30597375+acarbutt@users.noreply.github.com>
2024-01-12 16:55:45 +0000

    ukmcab-1246-edit lock for return url (#482)
    * edit lock for return url
    
    * only show edit button if not edit locked

--- src/UKMCAB.Data/Models/Audit.cs
+++ src/UKMCAB.Data/Models/Audit.cs
@@ -91,8 +91,8 @@ namespace UKMCAB.Data.Models
         public const string Published = nameof(Published);
         public const string RePublished = nameof(RePublished);
         public const string Archived = nameof(Archived); 
-        public const string Unarchived = nameof(Unarchived); // Used for a draft that is back from the dead
-        public const string UnarchiveRequest = nameof(UnarchiveRequest); //Used for CAB's that have been unarchived status archived (previous version)
+        public const string Unarchived = nameof(Unarchived); // Used on a draft that has been unarchived
+        public const string UnarchiveRequest = nameof(UnarchiveRequest); //Used for CAB's that have been unarchived (previous and new draft version)
         public const string UnarchiveApprovalRequest = "Unarchive CAB approval request"; // Ukas request for unarchive
         public const string SubmittedForApproval = "Submitted for OPSS approval";
         public const string CABApproved = "CAB Approved";
