65add753442cd608aa8ab55c3d658d2fb8c41311
Gopi <gopi.govind@olivejar.co.uk>
2024-01-17 11:33:53 +0000

    UKMCAB-1504 - CSS class fix (#493)
    CSS class fix

--- src/UKMCAB.Web.UI/Areas/Search/Controllers/CABProfileController.cs
+++ src/UKMCAB.Web.UI/Areas/Search/Controllers/CABProfileController.cs
@@ -223,6 +223,7 @@ namespace UKMCAB.Web.UI.Areas.Search.Controllers
                 RegisteredTestLocations = cabDocument.TestingLocations ?? new List<string>(),
                 Status = cabDocument.Status,
                 SubStatus = cabDocument.SubStatus.GetEnumDescription(),
+                StatusCssStyle = CssClassUtils.CabStatusStyle(cabDocument.StatusValue),
                 LegislativeAreas = cabDocument.LegislativeAreas ?? new List<string>(),
                 ProductSchedules = new CABDocumentsViewModel
                 {
@@ -283,7 +284,6 @@ namespace UKMCAB.Web.UI.Areas.Search.Controllers
                 summaryBreak.HasValue ? task?.Body.Substring(0, summaryBreak.Value) : null;
             profileViewModel.UnarchiveReason = task?.Body;
             profileViewModel.UnarchiveTaskType = task?.TaskType;
-
             return profileViewModel;
         }
 
