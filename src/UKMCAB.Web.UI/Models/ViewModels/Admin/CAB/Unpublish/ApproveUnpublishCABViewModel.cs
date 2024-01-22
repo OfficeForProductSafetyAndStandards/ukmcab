779ed4730d18094a5c5a2fcc99e3d1109771f2f9
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-01-22 17:14:54 +0000

    Update ApproveUnpublishCABViewModel.cs

--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/Unpublish/ApproveUnpublishCABViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/Unpublish/ApproveUnpublishCABViewModel.cs
@@ -9,5 +9,6 @@ public record ApproveUnpublishCABViewModel(
     Guid CabId) : BasicPageModel(Title)
 {
     [Required(ErrorMessage = "Enter the reason for approving the request to unpublish this CAB")]
+    [MaxLength(1000, ErrorMessage = "Maximum reason length is 1000 characters")]
     public string? Reason { get; set; }
 }
