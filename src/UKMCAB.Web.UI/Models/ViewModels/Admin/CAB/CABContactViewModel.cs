3e3de37ee397362c09dcc741e08f18f22113bcba
Robert Smith <robert.smith@olivejar.co.uk>
2024-01-23 11:48:47 +0000

    1469: Basic validation added to CAB create screen, fix to date year validation (4 digits needed), plus small updates to date validation errors (remove full stops).

--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABContactViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using UKMCAB.Data.Models;
+using static UKMCAB.Data.DataConstants;
 
 namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB
 {
@@ -35,12 +36,19 @@ namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB
 
         [Required(ErrorMessage = "Enter an address")]
         public string? AddressLine1 { get; set; }
+
         public string? AddressLine2 { get; set; }
+
         [Required(ErrorMessage = "Enter a town or city")]
         public string? TownCity { get; set; }
+
         [Required(ErrorMessage = "Enter a postcode")]
+        [MaxLength(20, ErrorMessage = "Maximum postcode length is 20 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9().\-\ ]+$", ErrorMessage = "Enter a postcode in the correct format")]
         public string? Postcode { get; set; }
+
         public string? County { get; set; }
+
         [Required(ErrorMessage = "Enter a country")]
         public string? Country { get; set; }
 
@@ -48,19 +56,34 @@ namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB
             new [] { AddressLine1, AddressLine2, TownCity, County, Postcode, Country }.Where(a =>
                 !string.IsNullOrWhiteSpace(a)));
 
+        [RegularExpression(@"^http(s)?://([\w-]+.)+[\w-]+(/[\w- ./?%&=])?$", ErrorMessage = "Enter a URL in the correct format")]
         public string? Website { get; set; }
+
+        [RegularExpression("^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$", ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         public string? Email { get; set; }
+
         [Required(ErrorMessage = "Enter a telephone number")]
+        [MaxLength(20, ErrorMessage = "Maximum telephone number length is 20 characters")]
+        [RegularExpression(@"^((\+\d{1,3}\s?)|0)[\d\s()-]{9,15}$", ErrorMessage = "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192")]
         public string? Phone { get; set; }
+
         public string? PointOfContactName { get; set; }
+
+        [RegularExpression("^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,})$", ErrorMessage = "Enter an email address in the correct format, like name@example.com")]
         public string? PointOfContactEmail { get; set; }
+
+        [MaxLength(20, ErrorMessage = "Maximum telephone number length is 20 characters")]
+        [RegularExpression(@"^((\+\d{1,3}\s?)|0)[\d\s()-]{9,15}$", ErrorMessage = "Enter a telephone number, like 01632 960 001, 07700 900 982 or +44 808 157 0192")]
         public string? PointOfContactPhone { get; set; }       
+
         public bool? IsPointOfContactPublicDisplay { get; set; }
+
         [Required(ErrorMessage = "Enter a registered office location")]
         public string? RegisteredOfficeLocation { get; set; }
 
         public string? Title => "Contact details";
 
-        public string[] FieldOrder => new[] { nameof(AddressLine1), nameof(TownCity), nameof(Postcode), nameof(Country), nameof(Email), nameof(Phone), nameof(IsPointOfContactPublicDisplay), nameof(RegisteredOfficeLocation) };
+        public string[] FieldOrder => new[] { nameof(AddressLine1), nameof(TownCity), nameof(Postcode), nameof(Country), nameof(Website), nameof(Email), nameof(Phone), 
+            nameof(PointOfContactEmail), nameof(PointOfContactPhone), nameof(IsPointOfContactPublicDisplay), nameof(RegisteredOfficeLocation) };
     }
 }
