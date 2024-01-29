4bc51d0ab46f8c4184c31b77d31d8c1426394230
Robert Smith <robert.smith@olivejar.co.uk>
2024-01-29 14:55:39 +0000

    1541: New classes added for data model reference data.

---
+++ src/UKMCAB.Data/Models/LegislativeAreas/Procedure.cs
@@ -0,0 +1,17 @@
+﻿namespace UKMCAB.Data.Models.LegislativeAreas
+{
+    public class Procedure
+    {
+        public Guid Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid LegislativeAreaId { get; set; }
+
+        public Guid PurposeOfAppointmentId { get; set; }
+
+        public List<Guid> CategoryIds { get; set; }
+
+        public List<Guid> ProductIds { get; set; }
+    }
+}
