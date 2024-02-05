e380bbad8c015ec7c439be2cb219c89e28a02262
Gopi <gopi.govind@olivejar.co.uk>
2024-02-05 13:10:51 +0000

    Validation added

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaAdditionalInformationController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaAdditionalInformationController.cs
@@ -10,18 +10,32 @@ public class LegislativeAreaAdditionalInformationController : Controller
     public static class Routes
     {
         public const string LegislativeAreaAdditionalInformation = "legislative.area.additional.information";
-        
+
     }
+
     public LegislativeAreaAdditionalInformationController()
     {
-         
+
     }
     
+    //TODO: work in-progress
     [HttpGet("additional-information", Name = Routes.LegislativeAreaAdditionalInformation)]
     public async Task<IActionResult> DetailsAsync()
     {
         var vm = new LegislativeAreaAdditionalInformationViewModel(Title: "Legislative area: additional information");
-        
+
         return View("~/Areas/Admin/views/CAB/LegislativeArea/AdditionalInformation.cshtml", vm);
+    }
+
+    //TODO: work in-progress
+    [HttpPost("additional-information", Name = Routes.LegislativeAreaAdditionalInformation)]
+    public async Task<IActionResult> DetailsAsync(LegislativeAreaAdditionalInformationViewModel vm)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View("~/Areas/Admin/views/CAB/LegislativeArea/AdditionalInformation.cshtml", vm);
+        }
+
+        return null;
     }
 }
--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/LegislativeArea/LegislativeAreaAdditionalInformationViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/LegislativeArea/LegislativeAreaAdditionalInformationViewModel.cs
@@ -1,11 +1,12 @@
 using UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.Shared;
-
+using System.ComponentModel.DataAnnotations;
 namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.LegislativeArea;
 
 public record LegislativeAreaAdditionalInformationViewModel(
     string? Title
 ) : BasicPageModel(Title)
 {
+    [Required(ErrorMessage = "Select if this is a provisional legislative area")]
     public bool? IsProvisionalLegislativeArea { get; init; }
     public HelperDate? AppointmentDate { get; init; }
     public HelperDate? ReviewDate { get; init; }
