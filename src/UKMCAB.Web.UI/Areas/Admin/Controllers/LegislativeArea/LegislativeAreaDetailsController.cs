3c73af6953d4660d67a81401b5769f255dc8a15b
Jignesh Jethwani <154232898+jjethwani-coder@users.noreply.github.com>
2024-02-06 15:22:59 +0000

    Story/ukmcab 1268 (#572)
    * ukmcab-1268 - added legislative area saved functionality
    
    * resolve conflict
    
    * removed nullable legislative id in document scope of appointment selection

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaDetailsController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaDetailsController.cs
@@ -2,6 +2,9 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using UKMCAB.Core.Services.CAB;
 using UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.LegislativeArea;
+using UKMCAB.Data.Models;
+using System.Security.Claims;
+using UKMCAB.Core.Services.Users;
 
 namespace UKMCAB.Web.UI.Areas.Admin.Controllers.LegislativeArea;
 
@@ -12,6 +15,7 @@ public class LegislativeAreaDetailsController : Controller
     private readonly ICABAdminService _cabAdminService;
     private readonly IEditLockService _editLockService;
     private readonly ILegislativeAreaService _legislativeAreaService;
+    private readonly IUserService _userService;
 
     public static class Routes
     {
@@ -22,12 +26,14 @@ public class LegislativeAreaDetailsController : Controller
     public LegislativeAreaDetailsController(
         ICABAdminService cabAdminService,            
         IEditLockService editLockService,
-        ILegislativeAreaService legislativeAreaService
+        ILegislativeAreaService legislativeAreaService,
+        IUserService userService
     )
     {
         _cabAdminService = cabAdminService;        
         _editLockService = editLockService;
         _legislativeAreaService = legislativeAreaService;
+        _userService = userService;
     }
     
     [HttpGet("details/{laId}", Name = Routes.LegislativeAreaDetails)]
@@ -41,8 +47,6 @@ public class LegislativeAreaDetailsController : Controller
     [HttpGet("add", Name = Routes.LegislativeAreaAdd)]
     public async Task<IActionResult> AddLegislativeArea(Guid id, string? returnUrl)
     {
-        var legislativeareas = await _legislativeAreaService.GetAllLegislativeAreasAsync();
-
         var vm = new LegislativeAreaViewModel
         {
             CABId = id,
@@ -57,23 +61,58 @@ public class LegislativeAreaDetailsController : Controller
     public async Task<IActionResult> AddLegislativeArea(Guid id, LegislativeAreaViewModel vm, string submitType)
     {
         if (ModelState.IsValid)
-        {
-            // todo save legislative area/scope of appointment;
+        {   
+            var latestDocument = await _cabAdminService.GetLatestDocumentAsync(id.ToString());
 
-            if (submitType == Constants.SubmitType.Continue)
+            // Implies no document or archived
+            if (latestDocument == null)
             {
-                return RedirectToRoute(Routes.LegislativeAreaDetails, new { id });
+                return RedirectToAction("CABManagement", "CabManagement", new { Area = "admin" });
             }
-            // save additional info
-            else if (submitType == Constants.SubmitType.AdditionalInfo)
-            {
-                return RedirectToRoute(Routes.LegislativeAreaDetails, new { id });
-            }
-            // save as draft
             else
             {
-                return RedirectToAction("Summary", "CAB", new { Area = "admin", id, subSectionEditAllowed = true });
-            }
+                // add  document new legislative area;
+                var documentLegislativeAreaId = Guid.NewGuid();
+
+                var documentLegislativeArea = new DocumentLegislativeArea
+                {
+                    Id = documentLegislativeAreaId,
+                    LegislativeAreaId = vm.SelectedLegislativeAreaId
+                };
+
+                latestDocument.DocumentLegislativeAreas.Add(documentLegislativeArea);
+
+                // add new document scope of appointment;
+                var scopeOfAppointmentId = Guid.NewGuid();
+
+                var scopeOfAppointment = new DocumentScopeOfAppointment
+                {
+                    Id = scopeOfAppointmentId,
+                    LegislativeAreaId = vm.SelectedLegislativeAreaId
+                };
+
+                latestDocument.ScopeOfAppointments.Add(scopeOfAppointment);
+
+                var userAccount =
+                  await _userService.GetAsync(User.Claims.First(c => c.Type.Equals(ClaimTypes.NameIdentifier)).Value);
+
+                await _cabAdminService.UpdateOrCreateDraftDocumentAsync(userAccount, latestDocument);
+
+                if (submitType == Constants.SubmitType.Continue)
+                {
+                    return RedirectToRoute(Routes.PurposeOfAppointment, new { id, scopeId = scopeOfAppointmentId });
+                }
+                // save additional info
+                else if (submitType == Constants.SubmitType.AdditionalInfo)
+                {
+                    return RedirectToRoute(Routes.LegislativeAreaDetails, new { id, laId = documentLegislativeAreaId });
+                }
+                // save as draft
+                else
+                {
+                    return RedirectToAction("Summary", "CAB", new { Area = "admin", id, subSectionEditAllowed = true });
+                }
+            }            
         }
         else
         {
