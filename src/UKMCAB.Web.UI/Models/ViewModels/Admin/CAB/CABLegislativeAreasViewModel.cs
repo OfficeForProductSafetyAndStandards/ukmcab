9d4ba4163f42b0429649ff6bbb5fa74ed662c6bc
Robert Smith <robert.smith@olivejar.co.uk>
2024-02-12 18:35:58 +0000

    1377: Tweak to LA section validation rule.

--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABLegislativeAreasViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABLegislativeAreasViewModel.cs
@@ -13,15 +13,17 @@ public class CABLegislativeAreasViewModelValidator : AbstractValidator<CABLegisl
 {
     public CABLegislativeAreasViewModelValidator()
     {
-        // There must be at least one legislative area.
-        // If the LA has a data model (scope of appointment can be selected), then there must be at least one ScopeOfAppointment object with at least one procedure.
-        // If any ScopeOfAppointment doesn't have a procedure, or the procedure is null or empty, validation will fail.
+        // 1. There must be at least one legislative area.
+        // - Every LA must have the IsProvisional property set.
+        // - If the LA requires scope of appointment to be chosen (HasDataModel="1" in the LA container data), then there
+        //   must be at least one ScopeOfAppointment object with at least one procedure.
+        //   If any ScopeOfAppointment doesn't have a procedure, or the procedure is null or empty, validation will fail.
 
         RuleFor(x => x.LegislativeAreas)
             .Must((model, legislativeAreas) =>
             {
-                bool complete = legislativeAreas.Any() && legislativeAreas.All(x => !x.CanChooseScopeOfAppointment || 
-                        (x.ScopeOfAppointments.Any() && x.ScopeOfAppointments.All(y => y.Procedures != null && y.Procedures.Any() && y.Procedures.All(z => !string.IsNullOrEmpty(z)))));
+                bool complete = legislativeAreas.Any() && legislativeAreas.All(x => x.IsProvisional.HasValue && 
+                    (!x.CanChooseScopeOfAppointment || (x.ScopeOfAppointments.Any() && x.ScopeOfAppointments.All(y => y.Procedures != null && y.Procedures.Any() && y.Procedures.All(z => !string.IsNullOrEmpty(z))))));
                 return complete;
             })
             .WithMessage("Legislative areas are incomplete");
