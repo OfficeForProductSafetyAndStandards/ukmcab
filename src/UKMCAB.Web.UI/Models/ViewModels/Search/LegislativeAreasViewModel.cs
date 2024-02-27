6276186cbf9091867851491d71519a2dba7a5b0b
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-02-27 16:58:32 +0000

    Archived Tag

--- src/UKMCAB.Web.UI/Models/ViewModels/Search/LegislativeAreasViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Search/LegislativeAreasViewModel.cs
@@ -1,9 +1,10 @@
 namespace UKMCAB.Web.UI.Models.ViewModels.Search;
 
-public class LegislativeAreasViewModel
+public class LegislativeAreaViewModel
 {
     public Guid LegislativeAreaId { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public string? Regulation { get; set; }
     public bool IsProvisional { get; set; }
+    public bool IsArchived { get; set; }
 }
