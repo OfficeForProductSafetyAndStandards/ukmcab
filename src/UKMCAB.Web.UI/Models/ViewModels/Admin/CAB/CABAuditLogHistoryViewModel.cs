b7f29bf96265bcc49c57e92f41300f8787e76fd5
Robert Smith <robert.smith@olivejar.co.uk>
2024-02-28 12:06:26 +0000

    1475: Added audit log history section to cab summary screen and a new screen for displaying the list of entries.

---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/CABAuditLogHistoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using UKMCAB.Web.UI.Models.ViewModels.Shared;
+
+namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB;
+
+public class CABAuditLogHistoryViewModel : ILayoutModel
+{
+    public string? Title { get; set; } = "History";
+
+    public AuditLogHistoryViewModel AuditLogHistory { get; set; }
+
+    public string? ReturnUrl { get; set; }
+}
---
+++ src/UKMCAB.Web.UI/Models/ViewModels/Shared/AuditLogHistoryTableViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace UKMCAB.Web.UI.Models.ViewModels.Shared
+{
+    public class AuditLogHistoryTableViewModel
+    {
+        public AuditLogHistoryViewModel AuditLogHistory { get; set; }
+
+        public string? ReturnUrl { get; set; }
+    }
+}
