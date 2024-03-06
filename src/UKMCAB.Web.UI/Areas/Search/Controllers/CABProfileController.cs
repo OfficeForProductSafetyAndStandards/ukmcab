54fa64175fc92dea59751569ed4453d680187750
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-03-06 15:45:20 +0000

    Update CABProfileController.cs

--- src/UKMCAB.Web.UI/Areas/Search/Controllers/CABProfileController.cs
+++ src/UKMCAB.Web.UI/Areas/Search/Controllers/CABProfileController.cs
@@ -844,10 +844,10 @@ namespace UKMCAB.Web.UI.Areas.Search.Controllers
                     Email = cabDocument.Email,
                     Phone = cabDocument.Phone,
                     BodyNumber = cabDocument.CABNumber,
-                    BodyTypes = cabDocument.BodyTypes ?? new List<string>(),
+                    BodyTypes = cabDocument.BodyTypes,
                     RegisteredOfficeLocation = cabDocument.RegisteredOfficeLocation,
-                    RegisteredTestLocations = cabDocument.TestingLocations ?? new List<string>(),
-                    LegislativeAreas = cabDocument.LegislativeAreas ?? new List<string>(),
+                    RegisteredTestLocations = cabDocument.TestingLocations,
+                    LegislativeAreas = cabDocument.DocumentLegislativeAreas.Select(l => l.LegislativeAreaName).ToList(),
                     ProductSchedules = cabDocument.Schedules?.Select(pdf => new SubscriptionsCoreCabFileModel
                     {
                         BlobName = pdf.BlobName,
