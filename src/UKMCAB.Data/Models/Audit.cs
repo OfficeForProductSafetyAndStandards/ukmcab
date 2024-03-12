c1dcf473201d754b00a4ebf0887aa38e67014092
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-03-12 14:09:04 +0000

    Correct audit log for Scope of appointments

--- src/UKMCAB.Data/Models/Audit.cs
+++ src/UKMCAB.Data/Models/Audit.cs
@@ -1,15 +1,20 @@
 ﻿using Ganss.Xss;
 using System.Text;
 using System.Web;
+using UKMCAB.Common;
 using UKMCAB.Data.Models.Users;
 
 namespace UKMCAB.Data.Models
 {
     public class Audit
     {
-        public Audit() { }
+        public Audit()
+        {
+        }
 
-        public Audit(string userId, string username, string userrole, DateTime date, string action, string? comment = null, string? publicComment = null, bool isUserInputComment = true, bool isUserEnteredPublicComment = true)
+        public Audit(string userId, string username, string userrole, DateTime date, string action,
+            string? comment = null, string? publicComment = null, bool isUserInputComment = true,
+            bool isUserEnteredPublicComment = true)
         {
             UserId = userId;
             UserName = username;
@@ -22,9 +27,17 @@ namespace UKMCAB.Data.Models
             IsUserEnteredPublicComment = isUserEnteredPublicComment;
         }
 
-        public Audit(UserAccount? userAccount, string action, string? comment = null, string? publicComment = null, bool isUserInputComment = true, bool isUserEnteredPublicComment = true) : this(userAccount?.Id, $"{userAccount?.FirstName} {userAccount?.Surname}", userAccount?.Role, DateTime.UtcNow, action, comment, publicComment, isUserInputComment, isUserEnteredPublicComment) { }
+        public Audit(UserAccount? userAccount, string action, string? comment = null, string? publicComment = null,
+            bool isUserInputComment = true, bool isUserEnteredPublicComment = true) : this(userAccount?.Id,
+            $"{userAccount?.FirstName} {userAccount?.Surname}", userAccount?.Role, DateTime.UtcNow, action, comment,
+            publicComment, isUserInputComment, isUserEnteredPublicComment)
+        {
+        }
 
-        public Audit(UserAccount? userAccount, string action, Document publishedDocument, Document? previousDocument = null, string? comment = null, string? publicComment = null) : this(userAccount?.Id, $"{userAccount?.FirstName} {userAccount?.Surname}", userAccount?.Role, DateTime.UtcNow, action, comment, publicComment)
+        public Audit(UserAccount? userAccount, string action, Document publishedDocument,
+            Document? previousDocument = null, string? comment = null, string? publicComment = null) : this(
+            userAccount?.Id, $"{userAccount?.FirstName} {userAccount?.Surname}", userAccount?.Role, DateTime.UtcNow,
+            action, comment, publicComment)
         {
             var sbComment = new StringBuilder();
             var sbPublicComment = new StringBuilder();
@@ -44,8 +57,12 @@ namespace UKMCAB.Data.Models
 
             if (previousDocument == null)
             {
-                sbComment.AppendFormat("<p class=\"govuk-body\">Appointment date: {0}</p>", publishedDocument.AppointmentDate.HasValue ? publishedDocument.AppointmentDate.Value.ToString("dd/MM/yyyy") + " 12:00" : "Not provided");
-                sbComment.AppendFormat("<p class=\"govuk-body\">Publication date: {0} 12:00</p>", DateTime.UtcNow.ToString("dd/MM/yyyy"));
+                sbComment.AppendFormat("<p class=\"govuk-body\">Appointment date: {0}</p>",
+                    publishedDocument.AppointmentDate.HasValue
+                        ? publishedDocument.AppointmentDate.Value.ToString("dd/MM/yyyy") + " 12:00"
+                        : "Not provided");
+                sbComment.AppendFormat("<p class=\"govuk-body\">Publication date: {0} 12:00</p>",
+                    DateTime.UtcNow.ToString("dd/MM/yyyy"));
             }
             else
             {
@@ -57,18 +74,21 @@ namespace UKMCAB.Data.Models
                     {
                         var previousFileUploads = previousDocument.Documents ?? new List<FileUpload>();
                         var currentFileUploads = publishedDocument.Documents ?? new List<FileUpload>();
-                        CalculateChangesToScheduleOrDocument(publishedDocument, previousDocument, sbComment, previousFileUploads, currentFileUploads, docType);
+                        CalculateChangesToScheduleOrDocument(publishedDocument, previousDocument, sbComment,
+                            previousFileUploads, currentFileUploads, docType);
                     }
                     else
                     {
                         var previousFileUploads = previousDocument.Schedules ?? new List<FileUpload>();
                         var currentFileUploads = publishedDocument.Schedules ?? new List<FileUpload>();
-                        CalculateChangesToScheduleOrDocument(publishedDocument, previousDocument, sbPublicComment, previousFileUploads, currentFileUploads, docType);
+                        CalculateChangesToScheduleOrDocument(publishedDocument, previousDocument, sbPublicComment,
+                            previousFileUploads, currentFileUploads, docType);
                     }
                 }
-                
-                CalculateChangesToLegislativeAreas(previousDocument.DocumentLegislativeAreas, publishedDocument.DocumentLegislativeAreas, sbPublicComment);
-                
+
+                CalculateChangesToLegislativeAreas(previousDocument.DocumentLegislativeAreas,
+                    publishedDocument.DocumentLegislativeAreas, sbPublicComment);
+
                 CalculateChangesToScopeOfAppointments(previousDocument, publishedDocument, sbPublicComment);
             }
 
@@ -87,55 +107,67 @@ namespace UKMCAB.Data.Models
             }
 
             comment = !string.IsNullOrEmpty(comment) ? $"<p class=\"govuk-body\">{comment}</p>" : string.Empty;
-            publicComment = !string.IsNullOrEmpty(publicComment) ? $"<p class=\"govuk-body\">{publicComment}</p>" : string.Empty;
+            publicComment = !string.IsNullOrEmpty(publicComment)
+                ? $"<p class=\"govuk-body\">{publicComment}</p>"
+                : string.Empty;
 
             Comment = string.Join("", HttpUtility.HtmlEncode(comment), HttpUtility.HtmlEncode(sbComment.ToString()));
-            PublicComment = string.Join("", HttpUtility.HtmlEncode(publicComment), HttpUtility.HtmlEncode(sbPublicComment.ToString()));
+            PublicComment = string.Join("", HttpUtility.HtmlEncode(publicComment),
+                HttpUtility.HtmlEncode(sbPublicComment.ToString()));
         }
 
-        private static void CalculateChangesToScopeOfAppointments(Document previousDocument, Document publishedDocument, StringBuilder sb)
+        private static void CalculateChangesToScopeOfAppointments(Document previousDocument, Document publishedDocument,
+            StringBuilder sb)
         {
-            var previousLaNames = previousDocument.DocumentLegislativeAreas.ToDictionary(l => l.LegislativeAreaId, l => l.LegislativeAreaName);
-            var currentLaNames = publishedDocument.DocumentLegislativeAreas.ToDictionary(l => l.LegislativeAreaId, l => l.LegislativeAreaName);
-            var previousScopes = previousDocument.ScopeOfAppointments;
-            var currentScopes = publishedDocument.ScopeOfAppointments;
-            var newlyAddedScopes = currentScopes.Where(cs => previousScopes.All(ps => ps.LegislativeAreaId != cs.LegislativeAreaId)).GroupBy(s => s.LegislativeAreaId).ToList();
-            var removedScopes = previousScopes.Where(ps => currentScopes.All(cs => cs.LegislativeAreaId != ps.LegislativeAreaId)).GroupBy(s => s.LegislativeAreaId).ToList();
-
-            foreach (var ns in newlyAddedScopes)
-            {
-                sb.AppendFormat(
-                    "<p class=\"govuk-body\">{0} have been added to {1}</p>",
-                    ns.Count(), currentLaNames[ns.Key]);
-            }
+            var legislativeAreasRemained =
+                publishedDocument.DocumentLegislativeAreas.Where(l => previousDocument.DocumentLegislativeAreas.Select(p => p.LegislativeAreaId).Contains(l.LegislativeAreaId)).ToDictionary(l => l.LegislativeAreaId,
+                    l => l.LegislativeAreaName);
             
-            foreach (var rs in removedScopes)
-            {
-                sb.AppendFormat(
-                    "<p class=\"govuk-body\">{0} have been removed from {1}</p>",
-                    rs.Count(), previousLaNames[rs.Key]);
+            foreach (var la in legislativeAreasRemained)
+            { 
+                var previousScopes = previousDocument.ScopeOfAppointments.Where(sa => sa.LegislativeAreaId == la.Key).Select(s => s.Id).ToList();
+                var currentScopes = publishedDocument.ScopeOfAppointments.Where(sa => sa.LegislativeAreaId == la.Key).Select(s => s.Id).ToList();
+                var countScopesRemoved =
+                    previousScopes.Count(ps => !currentScopes.Contains(ps));
+                var countScopesAdded =
+                    currentScopes.Count(cs => !previousScopes.Contains(cs));
+                if (countScopesAdded > 0)
+                {
+                    sb.Append(
+                        $"<p class=\"govuk-body\">{countScopesAdded} scope of appointment{(legislativeAreasRemained.Count > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been added to {la.Value}</p>");
+                }
+
+                if (countScopesRemoved > 0)
+                {
+                    sb.Append(
+                        $"<p class=\"govuk-body\">{countScopesRemoved} scope of appointment{(legislativeAreasRemained.Count > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been removed from {la.Value}</p>");
+                }
             }
         }
-        
-        private static void CalculateChangesToLegislativeAreas(List<DocumentLegislativeArea> previousLAs, List<DocumentLegislativeArea> currentLAs, StringBuilder sb)
-        {
-            var newlyAddedLAs = currentLAs.Where(la => previousLAs.All(pla => pla.LegislativeAreaId != la.LegislativeAreaId));
-            var removedLAs = previousLAs.Where(la => currentLAs.All(cla => cla.LegislativeAreaId != la.LegislativeAreaId));
 
-            foreach (var la in newlyAddedLAs) 
+        private static void CalculateChangesToLegislativeAreas(List<DocumentLegislativeArea> previousLAs,
+            List<DocumentLegislativeArea> currentLAs, StringBuilder sb)
+        {
+            var newlyAddedLAs =
+                currentLAs.Where(la => previousLAs.All(pla => pla.LegislativeAreaId != la.LegislativeAreaId));
+            var removedLAs =
+                previousLAs.Where(la => currentLAs.All(cla => cla.LegislativeAreaId != la.LegislativeAreaId));
+
+            foreach (var la in newlyAddedLAs)
             {
                 sb.AppendFormat(
                     "<p class=\"govuk-body\">{0} was added to legislative area.</p>",
                     la.LegislativeAreaName);
             }
-            foreach (var la in removedLAs) 
+
+            foreach (var la in removedLAs)
             {
                 sb.AppendFormat(
                     "<p class=\"govuk-body\">{0} was removed from legislative area.</p>",
                     la.LegislativeAreaName);
             }
 
-            foreach(var currentLa in currentLAs)
+            foreach (var currentLa in currentLAs)
             {
                 var previousLa = previousLAs.FirstOrDefault(x => x.Id == currentLa.Id);
 
@@ -151,9 +183,10 @@ namespace UKMCAB.Data.Models
                     }
                 }
             }
-        }  
+        }
 
-        private static void CalculateChangesToScheduleOrDocument(Document publishedDocument, Document? previousDocument, StringBuilder sb, List<FileUpload> previousFileUploads, List<FileUpload> currentFileUploads, string docType)
+        private static void CalculateChangesToScheduleOrDocument(Document publishedDocument, Document? previousDocument,
+            StringBuilder sb, List<FileUpload> previousFileUploads, List<FileUpload> currentFileUploads, string docType)
         {
             var isSchedule = docType.Equals("Schedules");
             var docTypes = isSchedule ? "product schedules" : "supporting documents";
@@ -161,30 +194,44 @@ namespace UKMCAB.Data.Models
 
             var existingFileUploads = currentFileUploads
                 .Where(sch => previousFileUploads.Any(prev => prev.UploadDateTime.Equals(sch.UploadDateTime)));
-            var newFileUploads = currentFileUploads.Where(sch => previousFileUploads.All(prev => !prev.UploadDateTime.Equals(sch.UploadDateTime)));
-            var removedFileUploads = previousFileUploads.Where(sch => currentFileUploads.All(pub => !pub.UploadDateTime.Equals(sch.UploadDateTime)));
+            var newFileUploads = currentFileUploads.Where(sch =>
+                previousFileUploads.All(prev => !prev.UploadDateTime.Equals(sch.UploadDateTime)));
+            var removedFileUploads = previousFileUploads.Where(sch =>
+                currentFileUploads.All(pub => !pub.UploadDateTime.Equals(sch.UploadDateTime)));
 
             foreach (var fileupload in existingFileUploads)
             {
-                var previousFileUpload = previousFileUploads.Single(sch => sch.UploadDateTime.Equals(fileupload.UploadDateTime));
+                var previousFileUpload =
+                    previousFileUploads.Single(sch => sch.UploadDateTime.Equals(fileupload.UploadDateTime));
                 if (isSchedule)
                 {
                     if (!previousFileUpload.LegislativeArea.Equals(fileupload.LegislativeArea))
                     {
-                        sb.AppendFormat("<p class=\"govuk-body\">The legislative area {0} has been changed to {1} on this product schedule <a href=\"{2}\" target=\"_blank\" class=\"govuk-link\">{3}</a>.</p>", previousFileUpload.LegislativeArea, fileupload.LegislativeArea, ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType), fileupload.Label);
+                        sb.AppendFormat(
+                            "<p class=\"govuk-body\">The legislative area {0} has been changed to {1} on this product schedule <a href=\"{2}\" target=\"_blank\" class=\"govuk-link\">{3}</a>.</p>",
+                            previousFileUpload.LegislativeArea, fileupload.LegislativeArea,
+                            ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType),
+                            fileupload.Label);
                     }
                 }
                 else
                 {
                     if (!previousFileUpload.Category.Equals(fileupload.Category))
                     {
-                        sb.AppendFormat("<p class=\"govuk-body\">The category {0} has been changed to {1} on this supporting document <a href=\"{2}\" target=\"_blank\" class=\"govuk-link\">{3}</a>.</p>", previousFileUpload.Category, fileupload.Category, ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType), fileupload.Label);
+                        sb.AppendFormat(
+                            "<p class=\"govuk-body\">The category {0} has been changed to {1} on this supporting document <a href=\"{2}\" target=\"_blank\" class=\"govuk-link\">{3}</a>.</p>",
+                            previousFileUpload.Category, fileupload.Category,
+                            ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType),
+                            fileupload.Label);
                     }
                 }
-                
+
                 if (!previousFileUpload.Label.Equals(fileupload.Label))
                 {
-                    sb.AppendFormat("<p class=\"govuk-body\">The title of this {0} <a href=\"{1}\" target=\"_blank\" class=\"govuk-link\">{2}</a> has been changed from {3} to {4}.</p>", docTypeName, ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType), fileupload.Label, previousFileUpload.Label, fileupload.Label);
+                    sb.AppendFormat(
+                        "<p class=\"govuk-body\">The title of this {0} <a href=\"{1}\" target=\"_blank\" class=\"govuk-link\">{2}</a> has been changed from {3} to {4}.</p>",
+                        docTypeName, ScheduleOrDocumentLink(publishedDocument.CABId, fileupload.FileName, docType),
+                        fileupload.Label, previousFileUpload.Label, fileupload.Label);
                 }
             }
 
@@ -192,13 +239,16 @@ namespace UKMCAB.Data.Models
             {
                 sb.AppendFormat(
                     "<p class=\"govuk-body\"><a href=\"{0}\" target=\"_blank\" class=\"govuk-link\">{1}</a> was added to the {2} page.</p>",
-                    ScheduleOrDocumentLink(publishedDocument.CABId, newFileUpload.FileName, docType), newFileUpload.Label, docTypes);
+                    ScheduleOrDocumentLink(publishedDocument.CABId, newFileUpload.FileName, docType),
+                    newFileUpload.Label, docTypes);
             }
+
             foreach (var removedFileUpload in removedFileUploads)
             {
                 sb.AppendFormat(
                     "<p class=\"govuk-body\"><a href=\"{0}\" target=\"_blank\" class=\"govuk-link\">{1}</a> was removed from the {2} page.</p>",
-                    ScheduleOrDocumentLink(publishedDocument.CABId, removedFileUpload.FileName, docType), removedFileUpload.Label, docTypes);
+                    ScheduleOrDocumentLink(publishedDocument.CABId, removedFileUpload.FileName, docType),
+                    removedFileUpload.Label, docTypes);
             }
         }
 
@@ -208,6 +258,7 @@ namespace UKMCAB.Data.Models
             {
                 return $"/search/cab-schedule-view/{cabId}?file={fileName}&filetype=schedules";
             }
+
             return $"/search/cab-schedule-view/{cabId}?file={fileName}&filetype=documents";
         }
 
@@ -227,19 +278,29 @@ namespace UKMCAB.Data.Models
         public const string Created = nameof(Created);
         public const string Published = nameof(Published);
         public const string RePublished = nameof(RePublished);
-        public const string Archived = nameof(Archived); 
+        public const string Archived = nameof(Archived);
         public const string Unarchived = nameof(Unarchived); // Used on a draft that has been unarchived
-        public const string UnarchivedToDraft = "Unarchived to draft"; //Used for CAB's that have been unarchived (previous and new draft version)
+
+        public const string
+            UnarchivedToDraft =
+                "Unarchived to draft"; //Used for CAB's that have been unarchived (previous and new draft version)
+
         public const string UnarchiveApprovalRequest = "Request to unarchive"; // Ukas request for unarchive
         public const string SubmittedForApproval = "Submitted for OPSS approval"; // UKAS request to publish
         public const string CABApproved = "CAB Approved";
         public const string CABDeclined = "CAB Declined";
         public const string DraftDeleted = "Draft deleted";
         public const string ArchiveApprovalRequest = "Request to archive"; // UKAS request to archive
-        public const string UnArchiveApprovalRequestDeclined = "CAB unarchive declined";  // Ukas request for unarchive declined
-        public const string UnpublishApprovalRequest = "Request to unpublish"; // UKAS request to un publish and create draft
-        public const string UnpublishApprovalRequestDeclined = "Request to unpublish declined"; // UKAS request to un publish and create draft
 
+        public const string
+            UnArchiveApprovalRequestDeclined = "CAB unarchive declined"; // Ukas request for unarchive declined
+
+        public const string
+            UnpublishApprovalRequest = "Request to unpublish"; // UKAS request to un publish and create draft
+
+        public const string
+            UnpublishApprovalRequestDeclined =
+                "Request to unpublish declined"; // UKAS request to un publish and create draft
     }
 
     public class AuditUserActions
