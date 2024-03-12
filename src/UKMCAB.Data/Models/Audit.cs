44d0ade037a54cabaf31391eb90daba16bfc55e2
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-03-12 15:48:37 +0000

    Fix for plural

--- src/UKMCAB.Data/Models/Audit.cs
+++ src/UKMCAB.Data/Models/Audit.cs
@@ -134,13 +134,13 @@ namespace UKMCAB.Data.Models
                 if (countScopesAdded > 0)
                 {
                     sb.Append(
-                        $"<p class=\"govuk-body\">{countScopesAdded} scope of appointment{(legislativeAreasRemained.Count > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been added to {la.Value}</p>");
+                        $"<p class=\"govuk-body\">{countScopesAdded} scope of appointment{(countScopesAdded > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been added to {la.Value}</p>");
                 }
 
                 if (countScopesRemoved > 0)
                 {
                     sb.Append(
-                        $"<p class=\"govuk-body\">{countScopesRemoved} scope of appointment{(legislativeAreasRemained.Count > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been removed from {la.Value}</p>");
+                        $"<p class=\"govuk-body\">{countScopesRemoved} scope of appointment{(countScopesRemoved > 1 ? "s" : null)} {(legislativeAreasRemained.Count > 1 ? "have" : "has")} been removed from {la.Value}</p>");
                 }
             }
         }
