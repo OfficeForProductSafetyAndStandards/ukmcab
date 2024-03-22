97a2ec57fcb41e5467123c005612c841aa39f15a
Robert Smith <robert.smith@olivejar.co.uk>
2024-03-22 17:09:19 +0000

    1562: Notification and email for legislative area approval by OGD.

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaApproveController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaApproveController.cs
@@ -1,8 +1,15 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using Notify.Interfaces;
 using UKMCAB.Common.Exceptions;
 using UKMCAB.Core.Domain.LegislativeAreas;
+using UKMCAB.Core.Domain.Workflow;
+using UKMCAB.Core.EmailTemplateOptions;
+using UKMCAB.Core.Security;
 using UKMCAB.Core.Services.CAB;
 using UKMCAB.Core.Services.Users;
+using UKMCAB.Core.Services.Workflow;
+using UKMCAB.Data.Models.Users;
 
 namespace UKMCAB.Web.UI.Areas.Admin.Controllers.LegislativeArea;
 
@@ -11,6 +18,9 @@ public class LegislativeAreaApproveController : UI.Controllers.ControllerBase
 {
     private readonly ICABAdminService _cabAdminService;
     private readonly ILegislativeAreaService _legislativeAreaService;
+    private readonly IAsyncNotificationClient _notificationClient;
+    private readonly CoreEmailTemplateOptions _templateOptions;
+    private readonly IWorkflowTaskService _workflowTaskService;
 
     public static class Routes
     {
@@ -19,10 +29,16 @@ public class LegislativeAreaApproveController : UI.Controllers.ControllerBase
 
     public LegislativeAreaApproveController(ICABAdminService cabAdminService,
         ILegislativeAreaService legislativeAreaService,
-        IUserService userService) : base(userService)
+        IUserService userService,
+        IAsyncNotificationClient notificationClient,
+        IOptions<CoreEmailTemplateOptions> templateOptions,
+        IWorkflowTaskService workflowTaskService) : base(userService)
     {
         _cabAdminService = cabAdminService;
         _legislativeAreaService = legislativeAreaService;
+        _notificationClient = notificationClient;
+        _templateOptions = templateOptions.Value;
+        _workflowTaskService = workflowTaskService;
     }
 
     [HttpPost("approve", Name = Routes.LegislativeAreaApprove)]
@@ -38,12 +54,52 @@ public class LegislativeAreaApproveController : UI.Controllers.ControllerBase
         
         await _cabAdminService.ApproveLegislativeAreaAsync((await _userService.GetAsync(User.GetUserId()!))!, id, la.Id);
         TempData.Add(Constants.ApprovedLA, true);
+
+        await SendNotificationOfLegislativeAreaApprovalAsync(new Guid(document.CABId), document.Name, la.Name, CurrentUser);
+
         return RedirectToRoute(CABController.Routes.CabSummary, new { id, subSectionEditAllowed = true });
     }
 
     private async Task<IList<LegislativeAreaModel>> GetLegislativeAreasForUserAsync()
     {
         return (await _legislativeAreaService.GetLegislativeAreasByRoleId(UserRoleId)).ToList();
+    }
+
+    private async Task SendNotificationOfLegislativeAreaApprovalAsync(Guid cabId, string cabName, string legislativeAreaName, UserAccount userAccount)
+    {
+        var user = new User(userAccount.Id, userAccount.FirstName, userAccount.Surname,
+            userAccount.Role ?? throw new InvalidOperationException(),
+            userAccount.EmailAddress ?? throw new InvalidOperationException());
+
+        var personalisation = new Dictionary<string, dynamic?>
+            {
+                { "CABName", cabName },
+                { "CABUrl",
+                    UriHelper.GetAbsoluteUriFromRequestAndPath(HttpContext.Request,
+                        Url.RouteUrl(CABController.Routes.CabSummary, new { id = cabId }))
+                },
+                { "userGroup", user.UserGroup },
+                { "userName", user.FirstAndLastName },
+                { "legislativeAreaName", legislativeAreaName }
+            };
+
+        await _notificationClient.SendEmailAsync(_templateOptions.ApprovedBodiesEmail,
+            _templateOptions.NotificationLegislativeAreaApproved, personalisation);
+
+        await _workflowTaskService.CreateAsync(
+            new WorkflowTask(
+                TaskType.LegislativeAreaApproved,
+                user,
+                Roles.OPSS.Id,
+                null,
+                DateTime.Now,
+                $"{user.FirstAndLastName} from {user.UserGroup} has approved the {legislativeAreaName} legislative area.",
+                user,
+                DateTime.Now,
+                true,
+                null,
+                true,
+                cabId));
     }
 
 }
