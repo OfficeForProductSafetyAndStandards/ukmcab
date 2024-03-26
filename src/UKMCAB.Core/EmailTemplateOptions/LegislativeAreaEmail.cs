53a09d6e2712226e9791e9b3114d16967f144de7
Robert Smith <robert.smith@olivejar.co.uk>
2024-03-26 13:03:20 +0000

    1562: Removed fix for LA opss ogd email role Id, as not required.

--- src/UKMCAB.Core/EmailTemplateOptions/LegislativeAreaEmail.cs
+++ src/UKMCAB.Core/EmailTemplateOptions/LegislativeAreaEmail.cs
@@ -9,22 +9,3 @@ public class LegislativeAreaEmail
     public string dftp { get; set; } = null!;
     public string opss_ogd { get; set; } = null!;
 }
-
-/// <summary>
-/// Returns dictionary of OGD roleIds and email addresses. Converts "opss_ogd" role ID to "opss (ogd)".
-/// </summary>
-public static class LegislativeAreaEmailExtensions
-{
-    public static Dictionary<string, string> ToDictionary(this LegislativeAreaEmail emailAddresses)
-    {
-        return new Dictionary<string, string>
-        {
-            { "dluhc", emailAddresses.dluhc },
-            { "mhra", emailAddresses.mhra },
-            { "mcga", emailAddresses.mcga },
-            { "dftr", emailAddresses.dftr },
-            { "dftp", emailAddresses.dftp },
-            { "opss (ogd)", emailAddresses.opss_ogd },
-        };
-    }
-}
