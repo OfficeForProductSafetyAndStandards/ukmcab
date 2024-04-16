5a8a3fa660101154b45dcff2bbd5baef8990f8d4
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-04-16 10:40:33 +0100

    3.1 with hotfix

--- src/UKMCAB.Data/DataConstants.cs
+++ src/UKMCAB.Data/DataConstants.cs
@@ -4,7 +4,7 @@ public class DataConstants
 {
     public static class Version
     {
-        public const string Number = "v3-0";
+        public const string Number = "v3-2";
     }
 
     public static class CosmosDb
