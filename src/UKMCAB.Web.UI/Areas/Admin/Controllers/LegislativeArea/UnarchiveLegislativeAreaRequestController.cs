aaa6a160da0d92c9ecf4eb6cfb015a64cf2969b1
Anthony Carbutt <30597375+acarbutt@users.noreply.github.com>
2024-04-22 10:20:22 +0100

    add reason page

--- src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/UnarchiveLegislativeAreaRequestController.cs
+++ src/UKMCAB.Web.UI/Areas/Admin/Controllers/LegislativeArea/UnarchiveLegislativeAreaRequestController.cs
@@ -9,21 +9,23 @@ using UKMCAB.Web.UI.Services;
 namespace UKMCAB.Web.UI.Areas.Admin.Controllers.LegislativeArea;
 
 [Area("admin"), Route("admin/cab/{id}/legislative-area"), Authorize]
-public class UnarchiveLegislativeAreaRequestController: UI.Controllers.ControllerBase
+public class UnarchiveLegislativeAreaRequestController : UI.Controllers.ControllerBase
 {
     private readonly ICABAdminService _cabAdminService;
     private readonly IDistCache _distCache;
     private readonly ILegislativeAreaService _legislativeAreaService;
-    private readonly ILegislativeAreaDetailService _legislativeAreaDetailService;    
+    private readonly ILegislativeAreaDetailService _legislativeAreaDetailService;
     private const string CacheKey = "soa_create_{0}";
 
     public static class Routes
     {
         public const string UnarchiveLegislativeAreaRequest =
+            "legislative.area.unarchive-legislativearea-request";
+
+        public const string UnarchiveLegislativeAreaRequestReason =
             "legislative.area.unarchive-legislativearea-request-reason";
-        public const string UnarchiveLegislativeAreaRequestReason = "legislative.area.unarchive-legislativearea-request-reason";
     }
-    
+
     public UnarchiveLegislativeAreaRequestController(
         ICABAdminService cabAdminService,
         ILegislativeAreaService legislativeAreaService,
@@ -40,23 +42,22 @@ public class UnarchiveLegislativeAreaRequestController: UI.Controllers.Controlle
     [HttpGet("unarchive-request/{legislativeAreaId}", Name = Routes.UnarchiveLegislativeAreaRequest)]
     public async Task<IActionResult> UnarchiveLegislativeAreaRequest(Guid id, Guid legislativeAreaId, bool fromSummary)
     {
-        var legislativeArea = await _legislativeAreaService.GetLegislativeAreaByIdAsync(legislativeAreaId);
         var latestDocument = await _cabAdminService.GetLatestDocumentAsync(id.ToString());
         var vm = new UnarchiveLegislativeAreaRequestViewModel
         {
             CabId = id,
-            LegislativeArea =  await _legislativeAreaDetailService.PopulateCABLegislativeAreasItemViewModelAsync(latestDocument, legislativeAreaId),
+            LegislativeArea =
+                await _legislativeAreaDetailService.PopulateCABLegislativeAreasItemViewModelAsync(latestDocument,
+                    legislativeAreaId),
             FromSummary = fromSummary
         };
         return View("~/Areas/Admin/views/CAB/LegislativeArea/UnarchiveLegislativeAreaRequest.cshtml", vm);
     }
 
-    //todo:unarchive request reason
-    // [HttpGet("unarchive-request-reason/{legislativeAreaId}", Name = Routes.UnarchiveLegislativeAreaRequestReason)]
-    // public async Task<IActionResult> UnarchiveLegislativeAreaRequestReason(Guid id, Guid legislativeAreaId)
-    // {   
-    //     var legislativeArea = await _legislativeAreaService.GetLegislativeAreaByIdAsync(legislativeAreaId);                
-    //     
-    //     return View("~/Areas/Admin/views/CAB/LegislativeArea/UnarchiveLegislativeAreaRequestReason.cshtml", vm);
-    // }
+    [HttpGet("unarchive-request-reason/{legislativeAreaId}", Name = Routes.UnarchiveLegislativeAreaRequestReason)]
+    public IActionResult UnarchiveLegislativeAreaRequestReason(Guid id, Guid legislativeAreaId)
+    {
+        var vm = new UnarchiveLegislativeAreaRequestReasonViewModel();
+        return View("~/Areas/Admin/views/CAB/LegislativeArea/UnarchiveLegislativeAreaRequestReason.cshtml", vm);
+    }
 }
