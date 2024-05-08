eb79d936a237a7e48ad3a07dbb02e52d3e5bb0c7
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2024-05-08 12:23:30 +0100

    Changed CAB substatus to draft if there are no more pending approval actions

--- src/UKMCAB.Core/Services/CAB/CABAdminService.cs
+++ src/UKMCAB.Core/Services/CAB/CABAdminService.cs
@@ -224,7 +224,7 @@ namespace UKMCAB.Core.Services.CAB
             }
             else if (draft.StatusValue == Status.Draft)
             {
-                if (draft.DocumentLegislativeAreas.All(la => la.Status == LAStatus.Published || la.Status == LAStatus.Declined || la.Status == LAStatus.DeclinedByOpssAdmin))
+                if (draft.DocumentLegislativeAreas.All(la => la.Status == LAStatus.Published || la.Status == LAStatus.Declined || la.Status == LAStatus.DeclinedByOpssAdmin || la.Status == LAStatus.DeclinedToArchiveAndArchiveScheduleByOPSS || la.Status == LAStatus.DeclinedToArchiveAndRemoveScheduleByOPSS || la.Status == LAStatus.DeclinedToRemoveByOPSS || la.Status == LAStatus.DeclinedToUnarchiveByOPSS))
                 {
                     draft.SubStatus = SubStatus.None;
                 }
