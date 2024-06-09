4a6ef982e488cd7d3f42be6e5e990c3f860d2f22
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2024-06-09 20:25:50 +0100

    added unit tests to cover the 3 different scenarios of adding Procedures to scope of appointment

---
+++ src/UKMCAB.Web.UI.Tests/Areas/Admin/Controllers/LegislativeArea/LegislativeAreaDetailsControllerTests.cs
@@ -0,0 +1,189 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Threading.Tasks;
+using UKMCAB.Core.Services.CAB;
+using UKMCAB.Web.UI.Services;
+using UKMCAB.Data.Models;
+using System;
+using System.Collections.Generic;
+using UKMCAB.Core.Domain.LegislativeAreas;
+using UKMCAB.Infrastructure.Cache;
+using UKMCAB.Core.Services.Users;
+using UKMCAB.Web.UI.Areas.Admin.Controllers.LegislativeArea;
+using UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.LegislativeArea;
+using UKMCAB.Data.Models.Users;
+using Microsoft.AspNetCore.Http;
+
+namespace UKMCAB.Web.UI.Tests.Areas.Admin.Controllers.LegislativeArea
+{
+    [TestFixture]
+    public class LegislativeAreaDetailsControllerTests : ControllerBaseTestsBase
+    {
+        private Mock<IDistCache> _mockDistCache = null!;
+        private Mock<ICABAdminService> _mockCabAdminService = null!;
+        private Mock<ILegislativeAreaService> _mockLegislativeAreaService = null!;
+        private Mock<ILegislativeAreaDetailService> _mockLegislativeAreaDetailService = null!;
+        private Mock<IUserService> _mockUserService = null!;
+        private LegislativeAreaDetailsController _sut = null!;
+
+
+        [SetUp]
+        public void Setup()
+        {
+            _mockDistCache = new Mock<IDistCache>();
+            _mockCabAdminService = new Mock<ICABAdminService>();
+            _mockLegislativeAreaService = new Mock<ILegislativeAreaService>();
+            _mockLegislativeAreaDetailService = new Mock<ILegislativeAreaDetailService>();
+            _mockUserService = new Mock<IUserService>();
+
+            _sut = new LegislativeAreaDetailsController(_mockCabAdminService.Object, _mockLegislativeAreaService.Object, _mockLegislativeAreaDetailService.Object, _mockUserService.Object, _mockDistCache.Object)
+            {
+                ControllerContext = GetControllerContextWithUser()
+            };
+
+        }
+
+        [Test]
+        public async Task ShouldAddProceduresToSOA_When_TheyAreLinkedDirectlyToPurposeofAppointment()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var scopeId = Guid.NewGuid();
+            var compareScopeId = Guid.NewGuid();
+            var procedureId1 = Guid.NewGuid();
+            var procedureId2 = Guid.NewGuid();
+            var procedureIds = new List<Guid> { procedureId1, procedureId2 };
+            var vm = new ProcedureViewModel
+            {
+                SelectedProcedureIds = procedureIds
+            };
+            var submitType = Constants.SubmitType.Continue;
+            var userAccount = new UserAccount
+            {
+                Id = "Test id",
+                FirstName = "Test",
+                Surname = "User",
+                Role = "Test role"
+            };
+
+            var scopeOfAppointment = new DocumentScopeOfAppointment { Id = Guid.NewGuid(), CategoryIdAndProcedureIds = new() };
+            var lastestDocument = new Document
+            {
+                ScopeOfAppointments = new List<DocumentScopeOfAppointment> { scopeOfAppointment }
+            };
+
+            _mockDistCache.Setup(x => x.GetAsync<DocumentScopeOfAppointment>(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(scopeOfAppointment);
+            _mockDistCache.Setup(x => x.SetAsync(It.IsAny<string>(), It.IsAny<DocumentScopeOfAppointment>(), It.IsAny<TimeSpan>(), -1)).ReturnsAsync(scopeOfAppointment.Id.ToString());
+            _mockCabAdminService.Setup(ca => ca.GetLatestDocumentAsync(It.IsAny<string>())).ReturnsAsync(lastestDocument);
+
+            _mockLegislativeAreaService.Setup(la => la.GetAllLegislativeAreasAsync()).ReturnsAsync(new List<LegislativeAreaModel>());
+
+            _mockUserService.Setup(u => u.GetAsync(It.IsAny<string>())).ReturnsAsync(userAccount);
+            _mockCabAdminService.Setup(c => c.UpdateOrCreateDraftDocumentAsync(userAccount, lastestDocument, false)).ReturnsAsync(lastestDocument);
+
+            //Act
+            var result = await _sut.AddProcedure(id, scopeId, 0, 0, vm, compareScopeId, submitType);
+
+            //Assert
+            Assert.That(scopeOfAppointment.CategoryIdAndProcedureIds.Count, Is.EqualTo(1));
+            Assert.Contains(procedureId1, scopeOfAppointment.CategoryIdAndProcedureIds[0].ProcedureIds);
+            Assert.Contains(procedureId2, scopeOfAppointment.CategoryIdAndProcedureIds[0].ProcedureIds);
+        }
+
+        [Test]
+        public async Task ShouldAddProceduresToSOA_When_TheyAreLinkedDirectlyToProducts()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var scopeId = Guid.NewGuid();
+            var compareScopeId = Guid.NewGuid();
+            var procedureId1 = Guid.NewGuid();
+            var procedureId2 = Guid.NewGuid();
+            var procedureIds = new List<Guid> { procedureId1, procedureId2 };
+            var vm = new ProcedureViewModel
+            {
+                CurrentProductId = Guid.NewGuid(),
+                SelectedProcedureIds = procedureIds
+            };
+            var submitType = Constants.SubmitType.Continue;
+            var userAccount = new UserAccount
+            {
+                Id = "Test id",
+                FirstName = "Test",
+                Surname = "User",
+                Role = "Test role"
+            };
+
+            var scopeOfAppointment = new DocumentScopeOfAppointment { Id = Guid.NewGuid(), CategoryIdAndProcedureIds = new() };
+            var lastestDocument = new Document
+            {
+                ScopeOfAppointments = new List<DocumentScopeOfAppointment> { scopeOfAppointment }
+            };
+
+            _mockDistCache.Setup(x => x.GetAsync<DocumentScopeOfAppointment>(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(scopeOfAppointment);
+            _mockDistCache.Setup(x => x.SetAsync(It.IsAny<string>(), It.IsAny<DocumentScopeOfAppointment>(), It.IsAny<TimeSpan>(), -1)).ReturnsAsync(scopeOfAppointment.Id.ToString());
+            _mockCabAdminService.Setup(ca => ca.GetLatestDocumentAsync(It.IsAny<string>())).ReturnsAsync(lastestDocument);
+
+            _mockLegislativeAreaService.Setup(la => la.GetAllLegislativeAreasAsync()).ReturnsAsync(new List<LegislativeAreaModel>());
+
+            _mockUserService.Setup(u => u.GetAsync(It.IsAny<string>())).ReturnsAsync(userAccount);
+            _mockCabAdminService.Setup(c => c.UpdateOrCreateDraftDocumentAsync(userAccount, lastestDocument, false)).ReturnsAsync(lastestDocument);
+
+            //Act
+            var result = await _sut.AddProcedure(id, scopeId, 0, 0, vm, compareScopeId, submitType);
+
+            //Assert
+            Assert.That(scopeOfAppointment.ProductIdAndProcedureIds.Count, Is.AtLeast(1));
+            Assert.Contains(procedureId1, scopeOfAppointment.ProductIdAndProcedureIds[0].ProcedureIds);
+            Assert.Contains(procedureId2, scopeOfAppointment.ProductIdAndProcedureIds[0].ProcedureIds);
+        }
+
+        [Test]
+        public async Task ShouldAddProceduresToSOA_When_TheyAreLinkedDirectlyToProductCategories()
+        {
+            //Arrange
+            var id = Guid.NewGuid();
+            var scopeId = Guid.NewGuid();
+            var compareScopeId = Guid.NewGuid();
+            var procedureId1 = Guid.NewGuid();
+            var procedureId2 = Guid.NewGuid();
+            var procedureIds = new List<Guid> { procedureId1, procedureId2 };
+            var vm = new ProcedureViewModel
+            {
+                CurrentCategoryId = Guid.NewGuid(),
+                SelectedProcedureIds = procedureIds
+            };
+            var submitType = Constants.SubmitType.Continue;
+            var userAccount = new UserAccount
+            {
+                Id = "Test id",
+                FirstName = "Test",
+                Surname = "User",
+                Role = "Test role"
+            };
+
+            var scopeOfAppointment = new DocumentScopeOfAppointment { Id = Guid.NewGuid(), CategoryIdAndProcedureIds = new() };
+            var lastestDocument = new Document
+            {
+                ScopeOfAppointments = new List<DocumentScopeOfAppointment> { scopeOfAppointment }
+            };
+
+            _mockDistCache.Setup(x => x.GetAsync<DocumentScopeOfAppointment>(It.IsAny<string>(), It.IsAny<int>())).ReturnsAsync(scopeOfAppointment);
+            _mockDistCache.Setup(x => x.SetAsync(It.IsAny<string>(), It.IsAny<DocumentScopeOfAppointment>(), It.IsAny<TimeSpan>(), -1)).ReturnsAsync(scopeOfAppointment.Id.ToString());
+            _mockCabAdminService.Setup(ca => ca.GetLatestDocumentAsync(It.IsAny<string>())).ReturnsAsync(lastestDocument);
+
+            _mockLegislativeAreaService.Setup(la => la.GetAllLegislativeAreasAsync()).ReturnsAsync(new List<LegislativeAreaModel>());
+
+            _mockUserService.Setup(u => u.GetAsync(It.IsAny<string>())).ReturnsAsync(userAccount);
+            _mockCabAdminService.Setup(c => c.UpdateOrCreateDraftDocumentAsync(userAccount, lastestDocument, false)).ReturnsAsync(lastestDocument);
+
+            //Act
+            var result = await _sut.AddProcedure(id, scopeId, 0, 0, vm, compareScopeId, submitType);
+
+            //Assert
+            Assert.That(scopeOfAppointment.CategoryIdAndProcedureIds.Count, Is.AtLeast(1));
+            Assert.Contains(procedureId1, scopeOfAppointment.CategoryIdAndProcedureIds[0].ProcedureIds);
+            Assert.Contains(procedureId2, scopeOfAppointment.CategoryIdAndProcedureIds[0].ProcedureIds);
+        }
+    }
+}
