454dd86c9077222091a3eb90e0e9eb0b8764f88b
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2024-06-20 14:17:06 +0100

    Changed the wording of the error message

--- src/UKMCAB.Web.UI/Constants.cs
+++ src/UKMCAB.Web.UI/Constants.cs
@@ -66,7 +66,7 @@ public static class Constants
     public class ErrorMessages
     {
         public const string InvalidLoginAttempt = "Enter a valid email address and password";
-        public const string DuplicateEntry = "Duplicate entry - record could not be saved.";
+        public const string DuplicateEntry = "Duplicate entry - the previous entry could not be saved as it is a duplicate.";
     }
 }
 
