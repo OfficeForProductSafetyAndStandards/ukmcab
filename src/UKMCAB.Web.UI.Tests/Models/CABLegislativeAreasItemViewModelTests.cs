c32b26934927f9c1e18278da3d2afe585a9b5952
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2024-06-20 14:15:06 +0100

    implemented checks to indicate an la is in a complete state

---
+++ src/UKMCAB.Web.UI.Tests/Models/CABLegislativeAreasItemViewModelTests.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UKMCAB.Data.Models;
+
+
+namespace UKMCAB.Web.UI.Tests.Models
+{
+    [TestFixture]
+    public class CABLegislativeAreasItemViewModelTests
+    {
+        [Test]
+        public void CABLegislativeAreasItemViewModel_IsComplete_Should_Return_True()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
+    }
+}
