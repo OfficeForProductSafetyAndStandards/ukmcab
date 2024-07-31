196cab804bfa04d477810d6a838eee3dee05eee4
Andrei Negrea <Andrei.Negrea@triad.co.uk>
2024-07-31 16:39:53 +0100

    created DesignatedStardards json based on excel sheet; add new CosmoDB container for designated standards; populate id of DesignatedStandards.json

---
+++ src/UKMCAB.Data/Models/DesignatedStandard.cs
@@ -0,0 +1,12 @@
+﻿namespace UKMCAB.Data.Models
+{
+    public class DesignatedStandard
+    {
+        public Guid Id { get; set; }
+        public Guid LegislativeAreaId { get; set; }
+        public string Regulation { get; set; }
+        public string Title { get; set; }
+        public List<string> ReferenceNumber { get; set; }
+        public string NoticeOfPublicationReference { get; set; }
+    }
+}
