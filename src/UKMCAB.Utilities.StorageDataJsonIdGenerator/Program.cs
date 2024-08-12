8e216d176e55116fc03b7edcb052d8790f1e84ca
negreaa <127328640+negreaa@users.noreply.github.com>
2024-08-12 18:34:25 +0100

    UKMCAB-1711 Construction products data import (#1048)
    Co-authored-by: yusufkabir <39498125+yusufkabir@users.noreply.github.com>

---
+++ src/UKMCAB.Utilities.StorageDataJsonIdGenerator/Program.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Serialization;
+using Newtonsoft.Json;
+using UKMCAB.Data.Models;
+
+var serializerSettings = new JsonSerializerSettings
+{
+    ContractResolver = new DefaultContractResolver
+    {
+        NamingStrategy = new CamelCaseNamingStrategy()
+    },
+    Formatting = Formatting.Indented
+};
+
+try
+{
+    var path = Path.Combine(Directory.GetCurrentDirectory(), "DesignatedStandards.json");
+    var designatedStandardsJson = File.ReadAllText(path);
+
+    var designatedStandards = JsonConvert.DeserializeObject<List<DesignatedStandard>>(designatedStandardsJson);
+    if (designatedStandards == null)
+    {
+        Console.WriteLine("Error: file content is empty");
+        return;
+    }
+    designatedStandards.ForEach(d => d.Id = Guid.NewGuid());
+
+    File.WriteAllText(path, JsonConvert.SerializeObject(designatedStandards, serializerSettings));
+
+}
+catch (Exception exception)
+{
+    Console.WriteLine("Error: {0}", exception.Message);
+}
+
+Console.WriteLine("Done!");
