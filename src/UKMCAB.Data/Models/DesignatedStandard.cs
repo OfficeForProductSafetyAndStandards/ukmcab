1417659c3facdbd8f96113f8daf6f18120711a25
Andrei Negrea <Andrei.Negrea@triad.co.uk>
2024-08-12 16:49:42 +0100

    change title to name for DesignatedStandards.json

--- src/UKMCAB.Data/Models/DesignatedStandard.cs
+++ src/UKMCAB.Data/Models/DesignatedStandard.cs
@@ -5,7 +5,7 @@
         public Guid Id { get; set; }
         public Guid LegislativeAreaId { get; set; }
         public string Regulation { get; set; }
-        public string Title { get; set; }
+        public string Name { get; set; }
         public List<string> ReferenceNumber { get; set; }
         public string NoticeOfPublicationReference { get; set; }
     }
