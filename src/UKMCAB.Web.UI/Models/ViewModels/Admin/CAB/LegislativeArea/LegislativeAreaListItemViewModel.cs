aaab4cff71286dee24db6e925e5d6c0c2406495f
Richard Priddy <richard.priddy@triad.co.uk>
2024-08-14 06:19:18 +0100

    Include aria-label description for Legislative Area (#1058)

--- src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/LegislativeArea/LegislativeAreaListItemViewModel.cs
+++ src/UKMCAB.Web.UI/Models/ViewModels/Admin/CAB/LegislativeArea/LegislativeAreaListItemViewModel.cs
@@ -1,4 +1,6 @@
 ﻿
+using UKMCAB.Data.Models.LegislativeAreas;
+
 namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.LegislativeArea
 {
     public class LegislativeAreaListItemViewModel : LegislativeAreaBaseViewModel
@@ -20,13 +22,38 @@ namespace UKMCAB.Web.UI.Models.ViewModels.Admin.CAB.LegislativeArea
         public int NoOfProductsInScopeOfAppointment { get; set; }
         public List<DesignatedStandardReadOnlyViewModel>? DesignatedStandards { get; set; }
 
+        public string RadioDescription
+        {
+            get
+            {
+                var description = new List<string>();
+
+                if (!string.IsNullOrEmpty(Title))
+                    description.Add($"Title: {Title}");
+
+                if (!string.IsNullOrEmpty(PurposeOfAppointment))
+                    description.Add($"Purpose of appointment: {PurposeOfAppointment}");
+
+                if (!string.IsNullOrEmpty(Category))
+                    description.Add($"Category: {Category}");
+                if (!string.IsNullOrEmpty(SubCategory))
+                    description.Add($"Sub-category: {SubCategory}");
+
+                if (!string.IsNullOrEmpty(Product))
+                    description.Add($"Product: {Product}");
+
+
+                return string.Join(", ", description);
+            }
+        }
+
         public LegislativeAreaListItemViewModel() : base("Legislative area list item") { }
         public LegislativeAreaListItemViewModel(
-            Guid legislativeAreaId, 
-            string legislativeArea, 
-            string? purposeOfAppointment, 
-            string? category, 
-            string? subCategory, 
+            Guid legislativeAreaId,
+            string legislativeArea,
+            string? purposeOfAppointment,
+            string? category,
+            string? subCategory,
             Guid scopeId,
             string? product,
             List<string>? procedures = null,
