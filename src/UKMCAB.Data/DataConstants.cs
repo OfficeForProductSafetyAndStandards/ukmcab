a74783d594cf7fbe7c0bfc0630274b2210527887
Steve Brockton <Steve.Brockton@triad.co.uk>
2024-09-10 09:53:23 +0100

    Update existing CABs

--- src/UKMCAB.Data/DataConstants.cs
+++ src/UKMCAB.Data/DataConstants.cs
@@ -4,7 +4,7 @@ public static class DataConstants
 {
     public static class Version
     {
-        public const string Number = "v5-0";
+        public const string Number = "v5-2";
     }
 
     public static class CosmosDb
