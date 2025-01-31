2a3f85dffab277df851ce43ba8616f74376b43c4
Kabir Yusuf <yusufkabir@hotmail.co.uk>
2025-01-31 16:15:07 +0000

    update version number and release date

--- src/UKMCAB.Data/DataConstants.cs
+++ src/UKMCAB.Data/DataConstants.cs
@@ -4,7 +4,7 @@ public static class DataConstants
 {
     public static class Version
     {
-        public const string Number = "v5-5";
+        public const string Number = "v5-6";
     }
 
     public static class CosmosDb
