e8fca31154b4cb4acb9843ab60e58cfc5a6815ee
Richard Priddy <richard.priddy@triad.co.uk>
2025-05-02 12:17:30 +0100

    Implement auto-migration (#1228)

---
+++ src/UKMCAB.Data/PostgreSQL/PostgreAutoMigration.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace UKMCAB.Data.PostgreSQL;
+
+public static class PostgreAutoMigration
+{
+    public static void MigrateDatabase(IApplicationBuilder app)
+    {
+        ArgumentNullException.ThrowIfNull(app);
+
+        var iServiceScopeFactory = app.ApplicationServices.GetService<IServiceScopeFactory>();
+        if (iServiceScopeFactory == null)
+        {
+            throw new ApplicationException($"{nameof(IServiceScopeFactory)} not found");
+        }
+
+        using var scope = iServiceScopeFactory.CreateScope();
+
+        scope.ServiceProvider.GetRequiredService<ApplicationDataContext>().Database.Migrate();
+    }
+}
