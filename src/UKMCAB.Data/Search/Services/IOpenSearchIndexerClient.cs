d445de3635438a5f9d424128a6f2c40005fd0c4f
tobytryad <tobi.omojayogbe@triad.co.uk>
2025-05-08 12:11:36 +0100

    UKMCAB-2230: RandomSortValue generator fix

---
+++ src/UKMCAB.Data/Search/Services/IOpenSearchIndexerClient.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKMCAB.Data.Search.Services
+{
+    /// <summary>
+    /// TEMPORARY Open Search Indexer (update as required)
+    /// </summary>
+    public interface IOpenSearchIndexerClient
+    {
+        Task RunIndexerAsync(string indexerName, CancellationToken token = default);
+    }
+}
---
+++ src/UKMCAB.Data/Search/Services/OpenSearchIndexerClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UKMCAB.Data.Search.Services
+{
+    /// <summary>
+    /// TEMPORARY Open Search Indexer implementation (update as required)
+    /// </summary>
+    public class OpenSearchIndexerClient : IOpenSearchIndexerClient
+    {
+        public async Task RunIndexerAsync(string indexerName, CancellationToken token = default)
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
